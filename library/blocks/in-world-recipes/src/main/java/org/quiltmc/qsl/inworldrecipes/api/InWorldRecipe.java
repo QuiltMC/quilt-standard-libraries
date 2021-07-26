bd230fe0ec29a5f4db4593e48f634e6738fb934a
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-07-26 20:04:47 +0300

    De-recordify InWorldRecipe to allow for documentation
    Apparently you can't add javadoc to record components. What's the point

--- library/blocks/in-world-recipes/src/main/java/org/quiltmc/qsl/inworldrecipes/api/InWorldRecipe.java
+++ library/blocks/in-world-recipes/src/main/java/org/quiltmc/qsl/inworldrecipes/api/InWorldRecipe.java
@@ -10,6 +10,43 @@ import java.util.function.Predicate;
 /**
  * Represents an in-world recipe.
  */
-public final record InWorldRecipe(@NotNull Block targetBlock,
-								  @NotNull Predicate<ItemUsageContext> predicate,
-								  @NotNull Consumer<ItemUsageContext> action) { }
+public final class InWorldRecipe {
+	private final @NotNull Block targetBlock;
+	private final @NotNull Predicate<ItemUsageContext> predicate;
+	private final @NotNull Consumer<ItemUsageContext> action;
+
+	/**
+	 * Creates an in-world recipe.
+	 * @param targetBlock target block
+	 * @param predicate recipe predicate
+	 * @param action action the recipe executes when completed
+	 */
+	public InWorldRecipe(@NotNull Block targetBlock,
+						 @NotNull Predicate<ItemUsageContext> predicate,
+						 @NotNull Consumer<ItemUsageContext> action) {
+		this.targetBlock = targetBlock;
+		this.predicate = predicate;
+		this.action = action;
+	}
+
+	/**
+	 * {@return the target block of this recipe}
+	 */
+	public @NotNull Block targetBlock() {
+		return targetBlock;
+	}
+
+	/**
+	 * {@return the predicate of this recipe}
+	 */
+	public @NotNull Predicate<ItemUsageContext> predicate() {
+		return predicate;
+	}
+
+	/**
+	 * {@return the action of this recipe}
+	 */
+	public @NotNull Consumer<ItemUsageContext> action() {
+		return action;
+	}
+}
