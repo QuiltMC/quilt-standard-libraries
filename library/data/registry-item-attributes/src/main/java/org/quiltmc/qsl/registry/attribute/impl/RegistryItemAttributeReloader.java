e55701a5ccbbe988b403d976285bb2cb6b419b37
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-08-18 17:00:18 +0300

    god I suck at organizing code

--- library/data/registry-item-attributes/src/main/java/org/quiltmc/qsl/registry/attribute/impl/RegistryItemAttributeReloader.java
+++ library/data/registry-item-attributes/src/main/java/org/quiltmc/qsl/registry/attribute/impl/RegistryItemAttributeReloader.java
@@ -17,10 +17,12 @@ import net.minecraft.util.InvalidIdentifierException;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
@@ -37,55 +39,58 @@ public final class RegistryItemAttributeReloader implements SimpleResourceReload
 	public CompletableFuture<LoadedData> load(ResourceManager manager, Profiler profiler, Executor executor) {
 		return CompletableFuture.supplyAsync(() -> {
 			Map<RegistryItemAttribute<?, ?>, AttributeMap> attributeMaps = new HashMap<>();
-			for (Map.Entry<? extends RegistryKey<? extends Registry<?>>, ? extends Registry<?>> entry : Registry.REGISTRIES.getEntries()) {
-				Identifier registryId = entry.getKey().getValue();
 
-				// calculate the root path of this registry's attribute maps
+			for (var entry : Registry.REGISTRIES.getEntries()) {
+				Identifier registryId = entry.getKey().getValue();
 				String path = getAttributeMapPath(registryId);
-				// find all JSON files that match this registry's attribute map path
 				profiler.push(ID + "/finding_resources/" + path);
+
 				Collection<Identifier> jsonIds = manager.findResources("attributes/" + path, s -> s.endsWith(".json"));
 				if (jsonIds.isEmpty()) {
 					continue;
 				}
+
+				Registry<?> registry = entry.getValue();
+				BuiltinRegistryItemAttributeHolder<?> holder = RegistryItemAttributeHolderImpl.getBuiltin(registry);
+				processResources(manager, profiler, attributeMaps, jsonIds, registry, holder);
+
 				profiler.pop();
-
-				// grab the built-in holder so we can see what attributes are registered for this registry
-				BuiltinRegistryItemAttributeHolder<?> holder = RegistryItemAttributeHolderImpl.getBuiltin(entry.getValue());
-				for (Identifier jsonId : jsonIds) {
-					// get the attribute ID from the resource ID
-					Identifier attribId = getAttributeId(jsonId);
-					// get the matching attribute (fail if it doesn't exist)
-					RegistryItemAttribute<?, ?> attrib = holder.getAttribute(attribId);
-					if (attrib == null) {
-						LOGGER.warn("Unknown attribute {} (from {})", attribId, jsonId);
-						continue;
-					}
-
-					// get all the resources that match this resource ID
-					profiler.push(ID + "/getting_resources{" + jsonId + "}");
-					List<Resource> resources;
-					try {
-						resources = manager.getAllResources(jsonId);
-					} catch (IOException e) {
-						LOGGER.error("Failed to get all resources for {}", jsonId);
-						LOGGER.catching(Level.ERROR, e);
-						continue;
-					}
-
-					// grab an AttributeMap (utility structure that stores deserialized data), creating one if needed
-					AttributeMap attribMap = attributeMaps.computeIfAbsent(attrib,
-							id -> new AttributeMap(entry.getValue(), attrib));
-					// finally, tell AttributeMap "hey, process these resources for me please"
-					profiler.swap(ID + "/processing_resources{" + jsonId + "," + attribId + "}");
-					for (Resource resource : resources) {
-						attribMap.processResource(resource);
-					}
-					profiler.pop();
-				}
 			}
 			return new LoadedData(attributeMaps);
 		}, executor);
+	}
+
+	private void processResources(ResourceManager manager, Profiler profiler,
+								  Map<RegistryItemAttribute<?, ?>, AttributeMap> attributeMaps,
+								  Collection<Identifier> jsonIds, Registry<?> registry,
+								  BuiltinRegistryItemAttributeHolder<?> holder) {
+		for (var jsonId : jsonIds) {
+			Identifier attribId = getAttributeId(jsonId);
+			RegistryItemAttribute<?, ?> attrib = holder.getAttribute(attribId);
+			if (attrib == null) {
+				LOGGER.warn("Unknown attribute {} (from {})", attribId, jsonId);
+				continue;
+			}
+
+			profiler.swap(ID + "/getting_resources{" + jsonId + "}");
+
+			List<Resource> resources;
+			try {
+				resources = manager.getAllResources(jsonId);
+			} catch (IOException e) {
+				LOGGER.error("Failed to get all resources for {}", jsonId);
+				LOGGER.catching(Level.ERROR, e);
+				continue;
+			}
+
+			profiler.swap(ID + "/processing_resources{" + jsonId + "," + attribId + "}");
+
+			AttributeMap attribMap = attributeMaps.computeIfAbsent(attrib,
+					id -> new AttributeMap(registry, attrib));
+			for (var resource : resources) {
+				attribMap.processResource(resource);
+			}
+		}
 	}
 
 	@Override
@@ -107,6 +112,7 @@ public final class RegistryItemAttributeReloader implements SimpleResourceReload
 		String path = jsonId.getPath();
 		int lastSlash = path.lastIndexOf('/');
 		path = path.substring(lastSlash + 1);
+
 		int lastDot = path.lastIndexOf('.');
 		path = path.substring(0, lastDot);
 		return new Identifier(jsonId.getNamespace(), path);
@@ -114,17 +120,17 @@ public final class RegistryItemAttributeReloader implements SimpleResourceReload
 
 	protected record LoadedData(Map<RegistryItemAttribute<?, ?>, AttributeMap> attributeMaps) {
 		public void apply(Profiler profiler) {
-			// clear all attribute values from all registries, since we're reloading them now
 			profiler.push(ID + "/clear_attributes");
-			for (Map.Entry<? extends RegistryKey<? extends Registry<?>>, ? extends Registry<?>> entry : Registry.REGISTRIES.getEntries()) {
+
+			for (var entry : Registry.REGISTRIES.getEntries()) {
 				RegistryItemAttributeHolderImpl.getData(entry.getValue()).clear();
 			}
 
-			// apply all our AttributeMap values!
 			for (Map.Entry<RegistryItemAttribute<?, ?>, AttributeMap> entry : attributeMaps.entrySet()) {
 				profiler.swap(ID + "/apply_attribute{" + entry.getKey().getId() + "}");
 				applyOne(entry.getKey(), entry.getValue());
 			}
+
 			profiler.pop();
 		}
 
@@ -153,9 +159,7 @@ public final class RegistryItemAttributeReloader implements SimpleResourceReload
 		}
 
 		public void processResource(Resource resource) {
-			// deserialize the resource
 			JsonObject obj = JsonHelper.deserialize(new InputStreamReader(resource.getInputStream()));
-			// yoink some values
 			boolean replace = JsonHelper.getBoolean(obj, "replace", false);
 			JsonObject values = JsonHelper.getObject(obj, "values");
 			if (values == null) {
@@ -163,12 +167,11 @@ public final class RegistryItemAttributeReloader implements SimpleResourceReload
 				return;
 			}
 
-			// if "replace" is true, the data file wants us to clear _all_ entries from other files before it.
-			// so, do that!
+			// if "replace" is true, the data file wants us to clear all entries from other files before it
 			if (replace) {
 				map.clear();
 			}
-			// start inspecting the "values" object
+
 			for (Map.Entry<String, JsonElement> entry : values.entrySet()) {
 				// keys are IDs for items in the registry
 				// therefore, we check that keys are A) valid identifiers, and B) contained in the registry
@@ -186,8 +189,7 @@ public final class RegistryItemAttributeReloader implements SimpleResourceReload
 					continue;
 				}
 
-				// values are... well, values
-				// we use Mojang's weird optical hacks to parse them from JSON
+				// values are deserialized via the attribute's associated Codec instance
 				DataResult<?> parsedValue = attribute.getCodec().parse(JsonOps.INSTANCE, entry.getValue());
 				if (parsedValue.result().isEmpty()) {
 					if (parsedValue.error().isPresent()) {
@@ -200,7 +202,6 @@ public final class RegistryItemAttributeReloader implements SimpleResourceReload
 					LOGGER.error("Ignoring attribute value for '{}' in {} since it's invalid", id, resource.getId());
 				}
 
-				// we got both a key and a value? great, throw them on the map!
 				map.put(id, parsedValue.result().get());
 			}
 		}
