82155b295e2fe75b8b11cce1826058a294fa6708
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-08-18 14:01:20 +0300

    Make resource reloader async
    Values are now deserialized during the load phase rather than the apply phase to allow for better error messages

--- library/data/registry-item-attributes/src/main/java/org/quiltmc/qsl/registry/attribute/impl/AttributeReloader.java
+++ library/data/registry-item-attributes/src/main/java/org/quiltmc/qsl/registry/attribute/impl/AttributeReloader.java
@@ -9,12 +9,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.quiltmc.qsl.registry.attribute.api.RegistryItemAttribute;
-import org.quiltmc.qsl.resource.loader.api.reloader.SimpleSynchronousResourceReloader;
+import org.quiltmc.qsl.resource.loader.api.reloader.SimpleResourceReloader;
 import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.InvalidIdentifierException;
 import net.minecraft.util.JsonHelper;
+import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import java.io.IOException;
@@ -23,8 +24,10 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
-public final class AttributeReloader implements SimpleSynchronousResourceReloader {
+public final class AttributeReloader implements SimpleResourceReloader<Map<RegistryItemAttribute<?, ?>, AttributeReloader.AttributeMap>> {
 	private static final Logger LOGGER = LogManager.getLogger("AttributeReloader");
 	private static final Identifier ID = new Identifier("quilt", "attributes");
 
@@ -34,62 +37,69 @@ public final class AttributeReloader implements SimpleSynchronousResourceReloade
 	}
 
 	@Override
-	public void reload(ResourceManager manager) {
-		Map<RegistryItemAttribute<?, ?>, AttributeMap> attributeMaps = load(manager);
-		apply(attributeMaps);
-	}
-
-	private Map<RegistryItemAttribute<?, ?>, AttributeMap> load(ResourceManager manager) {
-		final Map<RegistryItemAttribute<?, ?>, AttributeMap> attributeMaps = new HashMap<>();
-		for (Map.Entry<? extends RegistryKey<? extends Registry<?>>, ? extends Registry<?>> entry : Registry.REGISTRIES.getEntries()) {
-			Identifier registryId = entry.getKey().getValue();
-			StringBuilder pathSB = new StringBuilder("attributes/");
-			if (!"minecraft".equals(registryId.getNamespace()))
-				pathSB.append(registryId.getNamespace()).append('/');
-			pathSB.append(registryId.getPath());
-			Collection<Identifier> jsonIds = manager.findResources(pathSB.toString(), s -> s.endsWith(".json"));
-			if (jsonIds.isEmpty())
-				continue;
-			RegistryItemAttributeHolderImpl<?> holder = RegistryItemAttributeHolderImpl.get(entry.getValue());
-			for (Identifier jsonId : jsonIds) {
-				Identifier attribId = getAttributeId(jsonId);
-				RegistryItemAttribute<?, ?> attrib = holder.getAttribute(attribId);
-				if (attrib == null) {
-					LOGGER.warn("Unknown attribute {} (from {})", attribId, jsonId);
+	public CompletableFuture<Map<RegistryItemAttribute<?, ?>, AttributeMap>> load(ResourceManager manager, Profiler profiler, Executor executor) {
+		return CompletableFuture.supplyAsync(() -> {
+			final Map<RegistryItemAttribute<?, ?>, AttributeMap> attributeMaps = new HashMap<>();
+			for (Map.Entry<? extends RegistryKey<? extends Registry<?>>, ? extends Registry<?>> entry : Registry.REGISTRIES.getEntries()) {
+				Identifier registryId = entry.getKey().getValue();
+				StringBuilder pathSB = new StringBuilder();
+				if (!"minecraft".equals(registryId.getNamespace()))
+					pathSB.append(registryId.getNamespace()).append('/');
+				pathSB.append(registryId.getPath());
+				profiler.push(ID + "/finding_resources/" + pathSB);
+				Collection<Identifier> jsonIds = manager.findResources("attributes/" + pathSB, s -> s.endsWith(".json"));
+				if (jsonIds.isEmpty())
 					continue;
-				}
-				List<Resource> resources;
-				try {
-					resources = manager.getAllResources(jsonId);
-				} catch (IOException e) {
-					LOGGER.error("Failed to get all resources for {}", jsonId);
-					LOGGER.catching(Level.ERROR, e);
-					continue;
-				}
-				AttributeMap attribMap = attributeMaps.computeIfAbsent(attrib, id -> new AttributeMap(entry.getValue()));
-				for (Resource resource : resources) {
-					attribMap.processResource(resource);
+				profiler.pop();
+				RegistryItemAttributeHolderImpl<?> holder = RegistryItemAttributeHolderImpl.get(entry.getValue());
+				for (Identifier jsonId : jsonIds) {
+					Identifier attribId = getAttributeId(jsonId);
+					RegistryItemAttribute<?, ?> attrib = holder.getAttribute(attribId);
+					if (attrib == null) {
+						LOGGER.warn("Unknown attribute {} (from {})", attribId, jsonId);
+						continue;
+					}
+					profiler.push(ID + "/getting_resources{" + jsonId + "}");
+					List<Resource> resources;
+					try {
+						resources = manager.getAllResources(jsonId);
+					} catch (IOException e) {
+						LOGGER.error("Failed to get all resources for {}", jsonId);
+						LOGGER.catching(Level.ERROR, e);
+						continue;
+					}
+					AttributeMap attribMap = attributeMaps.computeIfAbsent(attrib,
+							id -> new AttributeMap(entry.getValue(), attrib));
+					profiler.swap(ID + "/processing_resources{" + jsonId + "," + attribId + "}");
+					for (Resource resource : resources) {
+						attribMap.processResource(resource);
+					}
+					profiler.pop();
 				}
 			}
-		}
-		return attributeMaps;
+			return attributeMaps;
+		}, executor);
 	}
 
-	private void apply(Map<RegistryItemAttribute<?, ?>, AttributeMap> attributeMaps) {
-		for (Map.Entry<RegistryItemAttribute<?, ?>, AttributeMap> entry : attributeMaps.entrySet()) {
-			applyOne(entry.getKey(), entry.getValue());
-		}
+	@Override
+	public CompletableFuture<Void> apply(Map<RegistryItemAttribute<?, ?>, AttributeMap> data, ResourceManager manager, Profiler profiler, Executor executor) {
+		return CompletableFuture.runAsync(() -> {
+			for (Map.Entry<RegistryItemAttribute<?, ?>, AttributeMap> entry : data.entrySet()) {
+				profiler.push(ID + "/apply_attribute{" + entry.getKey().getId() + "}");
+				applyOne(entry.getKey(), entry.getValue());
+				profiler.pop();
+			}
+		}, executor);
 	}
 
 	@SuppressWarnings("unchecked")
 	private <R, T> void applyOne(RegistryItemAttribute<R, T> attrib, AttributeMap attribMap) {
 		Registry<R> registry = (Registry<R>) Registry.REGISTRIES.get(attrib.getRegistryKey().getValue());
+		assert registry != null : "huh";
 		RegistryItemAttributeHolderImpl<R> holder = RegistryItemAttributeHolderImpl.get(registry);
-		for (Map.Entry<Identifier, JsonElement> attribEntry : attribMap.map.entrySet()) {
+		for (Map.Entry<Identifier, Object> attribEntry : attribMap.map.entrySet()) {
 			R item = registry.get(attribEntry.getKey());
-			DataResult<T> value = attrib.getCodec().parse(JsonOps.INSTANCE, attribEntry.getValue());
-			T result = value.result().orElseThrow(() -> new RuntimeException("aaa failed to deserialize attribute value"));
-			holder.putValue(item, attrib, result);
+			holder.putValue(item, attrib, (T) attribEntry.getValue());
 		}
 	}
 
@@ -104,19 +114,21 @@ public final class AttributeReloader implements SimpleSynchronousResourceReloade
 
 	protected static final class AttributeMap {
 		private final Registry<?> registry;
-		public final Map<Identifier, JsonElement> map;
+		private final RegistryItemAttribute<?, ?> attribute;
+		public final Map<Identifier, Object> map;
 
-		public AttributeMap(Registry<?> registry) {
+		public AttributeMap(Registry<?> registry, RegistryItemAttribute<?, ?> attribute) {
 			this.registry = registry;
+			this.attribute = attribute;
 			map = new HashMap<>();
 		}
 
-		public void processResource(Resource rsrc) {
-			JsonObject obj = JsonHelper.deserialize(new InputStreamReader(rsrc.getInputStream()));
+		public void processResource(Resource resource) {
+			JsonObject obj = JsonHelper.deserialize(new InputStreamReader(resource.getInputStream()));
 			boolean replace = JsonHelper.getBoolean(obj, "replace", false);
 			JsonObject values = JsonHelper.getObject(obj, "values");
 			if (values == null) {
-				LOGGER.error("Missing 'values' element in {}, ignoring file", rsrc.getId());
+				LOGGER.error("Missing 'values' element in {}, ignoring file", resource.getId());
 				return;
 			}
 			if (replace)
@@ -126,14 +138,27 @@ public final class AttributeReloader implements SimpleSynchronousResourceReloade
 				try {
 					id = new Identifier(entry.getKey());
 				} catch (InvalidIdentifierException e) {
-					LOGGER.error("Invalid identifier in values of {}: '{}', ignoring", rsrc.getId(), entry.getKey());
+					LOGGER.error("Invalid identifier in values of {}: '{}', ignoring",
+							resource.getId(), entry.getKey());
 					LOGGER.catching(Level.ERROR, e);
 					continue;
 				}
 				if (!registry.containsId(id)) {
-					LOGGER.error("Unregistered identifier in values of {}: '{}', ignoring", rsrc.getId(), id);
+					LOGGER.error("Unregistered identifier in values of {}: '{}', ignoring", resource.getId(), id);
+					continue;
 				}
-				map.put(id, entry.getValue());
+				DataResult<?> parsedValue = attribute.getCodec().parse(JsonOps.INSTANCE, entry.getValue());
+				if (parsedValue.result().isEmpty()) {
+					if (parsedValue.error().isPresent()) {
+						LOGGER.error("Failed to parse value for attribute {} of registry item {}: {}",
+								attribute.getId(), id, parsedValue.error().get().message());
+					} else {
+						LOGGER.error("Failed to parse value for attribute {} of registry item {}: unknown error",
+								attribute.getId(), id);
+					}
+					LOGGER.error("Ignoring attribute value for '{}' in {} since it's invalid", id, resource.getId());
+				}
+				map.put(id, parsedValue.result().get());
 			}
 		}
 	}
