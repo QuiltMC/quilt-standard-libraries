a66aa3465d784df12bb2fd8c4f3a92f2506d52ee
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-08-30 21:31:20 +0300

    Fix missing import here

--- library/data/registry-entry-attributes/src/main/java/org/quiltmc/qsl/registry/attribute/impl/RegistryEntryAttributeReloader.java
+++ library/data/registry-entry-attributes/src/main/java/org/quiltmc/qsl/registry/attribute/impl/RegistryEntryAttributeReloader.java
@@ -40,6 +40,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
