543629bcc38a365357ead80a680269d52a661bbc
williambl <william@williambl.com>
2021-09-06 18:38:44 +0100

    Remove unneeded local captures

--- library/core/crash_info/src/main/java/org/quiltmc/qsl/crash/mixin/EntityMixin.java
+++ library/core/crash_info/src/main/java/org/quiltmc/qsl/crash/mixin/EntityMixin.java
@@ -23,11 +23,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-	@Inject(method = "populateCrashReport", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
+	@Inject(method = "populateCrashReport", at = @At("TAIL"))
 	void addCrashReportDetails(CrashReportSection section, CallbackInfo ci) {
 		CrashReportEvents.ENTITY_DETAILS.invoker().addDetails((Entity) (Object) this, section);
 	}
