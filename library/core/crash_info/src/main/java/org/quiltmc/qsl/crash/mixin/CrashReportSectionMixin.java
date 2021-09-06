6a23a8f7113781abd4d1d3c1b1d5c9fb3f5d49c4
William Bradford Larcombe <william@williambl.com>
2021-09-06 10:42:22 +0000

    Update library/core/crash-info/src/main/java/org/quiltmc/qsl/crash/mixin/CrashReportSectionMixin.java
    Co-authored-by: BasiqueEvangelist <basiqueevangelist@yandex.ru>

--- library/core/crash_info/src/main/java/org/quiltmc/qsl/crash/mixin/CrashReportSectionMixin.java
+++ library/core/crash_info/src/main/java/org/quiltmc/qsl/crash/mixin/CrashReportSectionMixin.java
@@ -29,7 +29,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(CrashReportSection.class)
 public abstract class CrashReportSectionMixin {
-	@Inject(method = "addBlockInfo", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
+	@Inject(method = "addBlockInfo", at = @At("TAIL"))
 	private static void addCrashReportDetails(CrashReportSection element, HeightLimitView world, BlockPos pos, BlockState state, CallbackInfo ci) {
 		CrashReportEvents.BLOCK_DETAILS.invoker().addDetails(world, pos, state, element);
 	}
