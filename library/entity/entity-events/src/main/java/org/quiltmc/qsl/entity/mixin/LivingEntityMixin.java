f4f70c4bb4b6aaa84090a18a6e19c1a1808bc3a1
williambl <william@williambl.com>
2021-10-15 14:39:30 +0100

    Rename AfterKilledOtherEntityCallback -> EntityKilledCallback

--- library/entity/entity-events/src/main/java/org/quiltmc/qsl/entity/mixin/LivingEntityMixin.java
+++ library/entity/entity-events/src/main/java/org/quiltmc/qsl/entity/mixin/LivingEntityMixin.java
@@ -1,10 +1,8 @@
 package org.quiltmc.qsl.entity.mixin;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.server.world.ServerWorld;
-import org.quiltmc.qsl.entity.api.event.AfterKilledOtherEntityCallback;
+import org.quiltmc.qsl.entity.api.event.EntityKilledCallback;
 import org.quiltmc.qsl.entity.api.event.TryReviveCallback;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 	@Inject(method = "onKilledBy", at = @At(value = "HEAD"))
-	private void onEntityKilledOther(LivingEntity adversary, CallbackInfo ci, Entity attacker) {
-		AfterKilledOtherEntityCallback.EVENT.invoker().afterKilledOtherEntity(((LivingEntity) (Object) this).world, attacker, (LivingEntity) (Object) this);
+	private void onEntityKilledOther(LivingEntity adversary, CallbackInfo ci) {
+		EntityKilledCallback.EVENT.invoker().onKilled(((LivingEntity) (Object) this).world, adversary, (LivingEntity) (Object) this);
 	}
 
 	@Inject(method = "tryUseTotem", at = @At("HEAD"), cancellable = true)
