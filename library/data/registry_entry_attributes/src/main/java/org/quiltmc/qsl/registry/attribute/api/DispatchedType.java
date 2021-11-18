3e7110cb2b9c9da4aba54296be729aa2a1752ebe
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-11-18 14:46:46 +0200

    Fix licenses AGAIN

--- library/data/registry_entry_attributes/src/main/java/org/quiltmc/qsl/registry/attribute/api/DispatchedType.java
+++ library/data/registry_entry_attributes/src/main/java/org/quiltmc/qsl/registry/attribute/api/DispatchedType.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2021 QuiltMC
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,7 +31,7 @@ import java.util.function.Function;
  * }</code></pre>
  *
  * Using the {@code createDispatched} method, you can create an attribute for a composable behavior:<pre><code>
- * public static final SimpleRegistry&lt;Codec&lt;? extends Behavior&gt;&gt; REGISTRY = new SimpleRegistry&lt;&gt;();
+ * public static final SimpleRegistry&lt;Codec&lt;? extends Behavior&gt;&gt; REGISTRY = new SimpleRegistry&lt;&gt;\();
  * public static final RegistryEntryAttribute&lt;Item, Behavior&gt; ATTRIBUTE =
  *     RegistryEntryAttribute.&lt;Item, Behavior&gt;dispatchedBuilder(Registry.ITEM, id("behavior"), REGISTRY::get).build();
  *
