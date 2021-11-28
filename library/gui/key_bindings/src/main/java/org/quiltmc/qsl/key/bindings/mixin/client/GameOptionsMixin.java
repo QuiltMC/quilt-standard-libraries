fb288875d589329ce6ccf74affd07bfe45587dde
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2021-11-28 11:26:57 -0300

    Begin Key Bindings API Oh, this is definitely not ready yet, I just needed an easy way to sync progress between computers

---
+++ library/gui/key_bindings/src/main/java/org/quiltmc/qsl/key/bindings/mixin/client/GameOptionsMixin.java
@@ -0,0 +1,36 @@
+package org.quiltmc.qsl.key.bindings.mixin.client;
+
+import java.io.File;
+
+import org.quiltmc.qsl.key.bindings.impl.KeyBindingRegistryImpl;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
+
+@Mixin(GameOptions.class)
+public class GameOptionsMixin {
+	@Mutable
+	@Shadow
+	@Final
+	public KeyBinding[] keysAll;
+
+	@Inject(
+			at = @At(
+				value = "INVOKE",
+				target = "Lnet/minecraft/client/option/GameOptions;load()V"
+			),
+			method = "<init>"
+	)
+	private void modifyAllKeys(MinecraftClient client, File file, CallbackInfo ci) {
+		KeyBindingRegistryImpl.setVanillaKeys(this.keysAll);
+		this.keysAll = KeyBindingRegistryImpl.getKeysArray();
+	}
+}
