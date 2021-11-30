440cfce5c9fba03b2f706191828a255d776777d3
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2021-11-29 22:10:22 -0300

    Begin dynamic key binding system It's alive!

--- library/gui/key_bindings/src/main/java/org/quiltmc/qsl/key/bindings/impl/KeyBindingRegistryImpl.java
+++ library/gui/key_bindings/src/main/java/org/quiltmc/qsl/key/bindings/impl/KeyBindingRegistryImpl.java
@@ -1,21 +1,52 @@
 package org.quiltmc.qsl.key.bindings.impl;
 
-import org.apache.commons.lang3.ArrayUtils;
-import org.lwjgl.glfw.GLFW;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.quiltmc.qsl.key.bindings.mixin.client.GameOptionsAccessor;
+
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 
 public class KeyBindingRegistryImpl {
+	private static Map<KeyBinding, Boolean> quiltKeys = new HashMap<>();
 	private static KeyBinding[] allVanillaKeys = new KeyBinding[] {};
-	private static KeyBinding[] keysArray = new KeyBinding[] {
-		new KeyBinding("key.hello_world", GLFW.GLFW_KEY_H, "key.categories.misc")
-	};
+
+	public static KeyBinding registerKeyBinding(KeyBinding key, boolean enabled) {
+		quiltKeys.put(key, enabled);
+		applyChanges();
+		return key;
+	}
+
+	public static void setActive(KeyBinding key, boolean newEnabled) {
+		quiltKeys.replace(key, newEnabled);
+		applyChanges();
+	}
 
 	public static void setVanillaKeys(KeyBinding[] allKeys) {
 		allVanillaKeys = allKeys;
 	}
 
-	public static KeyBinding[] getKeysArray() {
-		return ArrayUtils.addAll(allVanillaKeys, keysArray);
+	public static KeyBinding[] updateKeysArray() {
+		List<KeyBinding> enabledQuiltKeys = new ArrayList<>();
+		for (var entry : quiltKeys.entrySet()) {
+			if (entry.getValue()) {
+				enabledQuiltKeys.add(entry.getKey());
+			}
+		}
+
+		KeyBinding[] quiltKeysArray = enabledQuiltKeys.toArray(new KeyBinding[enabledQuiltKeys.size()]);
+		
+		return ArrayUtils.addAll(allVanillaKeys, quiltKeysArray);
+	}
+
+	public static void applyChanges() {
+		MinecraftClient client = MinecraftClient.getInstance();
+		if (client.options != null) {
+			((GameOptionsAccessor)(Object)client.options).setKeysAll(updateKeysArray());
+		}
 	}
 }
--- library/gui/key_bindings/src/main/java/org/quiltmc/qsl/key/bindings/mixin/client/GameOptionsMixin.java
+++ library/gui/key_bindings/src/main/java/org/quiltmc/qsl/key/bindings/mixin/client/GameOptionsMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,6 +18,7 @@ import net.minecraft.client.option.KeyBinding;
 
 @Mixin(GameOptions.class)
 public class GameOptionsMixin {
+	// While there is an injection on the constructor, this Mutable is necessary
 	@Mutable
 	@Shadow
 	@Final
@@ -30,7 +32,7 @@ public class GameOptionsMixin {
 			method = "<init>"
 	)
 	private void modifyAllKeys(MinecraftClient client, File file, CallbackInfo ci) {
-		KeyBindingRegistryImpl.setVanillaKeys(this.keysAll);
-		this.keysAll = KeyBindingRegistryImpl.getKeysArray();
+		KeyBindingRegistryImpl.setVanillaKeys(this.keysAll.clone());
+		this.keysAll = KeyBindingRegistryImpl.updateKeysArray();
 	}
 }
