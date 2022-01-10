49863d74ddb8690c28798d36d5cafe49bd18c7ff
Eli Orona <eliorona@live.com>
2022-01-10 12:50:52 -0800

    Add translation api and fix headers

--- library/item/item_group/src/main/java/org/quiltmc/qsl/itemgroup/api/QuiltItemGroup.java
+++ library/item/item_group/src/main/java/org/quiltmc/qsl/itemgroup/api/QuiltItemGroup.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2016, 2017, 2018, 2019 FabricMC
- * Copyright 2021 QuiltMC
+ * Copyright 2022 QuiltMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 
@@ -37,17 +38,19 @@ import net.minecraft.util.collection.DefaultedList;
  * Extensions for the {@link ItemGroup} class. Currently, the only extension is setting the icon with either an {@link ItemConvertible} or {@link ItemStack} after the item has been created ({@link QuiltItemGroup#setIcon(ItemConvertible)}, {@link QuiltItemGroup#setIcon(ItemStack)}).<br>
  *
  * A {@link QuiltItemGroup} can be directly created with {@link QuiltItemGroup#create(Identifier)} or {@link QuiltItemGroup#createWithIcon(Identifier, Supplier)}.<br>
- * A {@link Builder}, which is used to add specific {@link ItemStack}s, especially with {@link net.minecraft.nbt}, can be obtained with {@link QuiltItemGroup#builder(Identifier)}.
+ * A {@link Builder}, which is used to add specific {@link ItemStack}s, especially with {@link net.minecraft.nbt.NbtElement}s, can be obtained with {@link QuiltItemGroup#builder(Identifier)}.
  */
 public final class QuiltItemGroup extends ItemGroup {
 	private final Supplier<ItemStack> iconSupplier;
 	private @Nullable ItemStack icon;
 	private final @Nullable Consumer<List<ItemStack>> stacksForDisplay;
+	private final @Nullable Text displayText;
 
-	private QuiltItemGroup(int index, String id, Supplier<ItemStack> iconSupplier, @Nullable Consumer<List<ItemStack>> stacksForDisplay) {
+	private QuiltItemGroup(int index, String id, Supplier<ItemStack> iconSupplier, @Nullable Consumer<List<ItemStack>> stacksForDisplay, @Nullable Text displayText) {
 		super(index, id);
 		this.iconSupplier = iconSupplier;
 		this.stacksForDisplay = stacksForDisplay;
+		this.displayText = displayText;
 	}
 
 	/**
@@ -87,6 +90,11 @@ public final class QuiltItemGroup extends ItemGroup {
 		if (this.stacksForDisplay != null) {
 			this.stacksForDisplay.accept(stacks);
 		}
+	}
+
+	@Override
+	public Text getTranslationKey() {
+		return displayText == null ? super.getTranslationKey() : displayText;
 	}
 
 	/**
@@ -187,6 +195,7 @@ public final class QuiltItemGroup extends ItemGroup {
 		private final Identifier identifier;
 		private Supplier<ItemStack> iconSupplier = () -> ItemStack.EMPTY;
 		private Consumer<List<ItemStack>> stacksForDisplay;
+		private Text text;
 
 		private Builder(Identifier identifier) {
 			this.identifier = identifier;
@@ -215,11 +224,21 @@ public final class QuiltItemGroup extends ItemGroup {
 		}
 
 		/**
+		 * Set the {@link Text} used as the name when rendering the {@link ItemGroup}
+		 * @param text The {@link Text} to render as the name
+		 * @return {@code this}
+		 */
+		public Builder displayText(@NotNull Text text) {
+			this.text = text;
+			return this;
+		}
+
+		/**
 		 * @return a new {@link QuiltItemGroup}
 		 */
 		public QuiltItemGroup build() {
 			((ItemGroupExtensions) GROUPS[0]).quilt$expandArray();
-			return new QuiltItemGroup(GROUPS.length - 1, String.format("%s.%s", identifier.getNamespace(), identifier.getPath()), iconSupplier, stacksForDisplay);
+			return new QuiltItemGroup(GROUPS.length - 1, String.format("%s.%s", identifier.getNamespace(), identifier.getPath()), iconSupplier, stacksForDisplay, text);
 		}
 	}
 }
