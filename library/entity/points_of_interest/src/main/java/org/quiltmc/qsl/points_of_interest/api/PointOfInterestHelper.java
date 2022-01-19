ccd9a025bdce4293d1c53ccbfc0d1a233062788f
Platymemo <edwardhazelton+github@gmail.com>
2022-01-18 20:54:27 -0900

    Allowed the creation of unregistered Points of Interest, and clarified javadocs.

--- library/entity/points_of_interest/src/main/java/org/quiltmc/qsl/points_of_interest/api/PointOfInterestHelper.java
+++ library/entity/points_of_interest/src/main/java/org/quiltmc/qsl/points_of_interest/api/PointOfInterestHelper.java
@@ -34,18 +34,18 @@ import org.quiltmc.qsl.points_of_interest.mixin.PointOfInterestTypeAccessor;
  * This class provides utilities to create a {@link PointOfInterestType}.
  *
  * <p>A point of interest is typically used by villagers to specify their workstation blocks, meeting zones and homes.
- * Points of interest are also used by bees to specify where their bee hive is and nether portals to find existing portals.
+ * Points of interest are also used by bees to specify where their bee hive is and nether portals to find existing portals.</p>
  */
 public final class PointOfInterestHelper {
 
 	/**
 	 * Creates and registers a {@link PointOfInterestType}.
 	 *
-	 * @param id The id of this {@link PointOfInterestType}.
-	 * @param ticketCount the amount of tickets.
-	 * @param searchDistance the search distance.
-	 * @param blocks all the blocks where a {@link PointOfInterest} of this type will be present.
-	 * @return a new {@link PointOfInterestType}.
+	 * @param id The id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
+	 * @param blocks all blocks where a {@link PointOfInterest} of this type will be present, will apply to all of the {@link Block}'s {@link BlockState}
+	 * @return a new {@link PointOfInterestType}
 	 */
 	public static PointOfInterestType register(Identifier id, int ticketCount, int searchDistance, Block... blocks) {
 		final ImmutableSet.Builder<BlockState> builder = ImmutableSet.builder();
@@ -54,18 +54,19 @@ public final class PointOfInterestHelper {
 			builder.addAll(block.getStateManager().getStates());
 		}
 
-		return register(id, ticketCount, searchDistance, builder.build());
+		return register(id, create(id, ticketCount, searchDistance, builder.build()));
 	}
 
 	/**
 	 * Creates and registers a {@link PointOfInterestType}.
 	 *
-	 * @param id The id of this {@link PointOfInterestType}.
-	 * @param ticketCount the amount of tickets.
-	 * @param completionCondition a {@link Predicate} which determines if two {@link PointOfInterestType}s are the same.
-	 * @param searchDistance the search distance.
-	 * @param blocks all blocks where a {@link PointOfInterest} of this type will be present
-	 * @return a new {@link PointOfInterestType}.
+	 * @param id The id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param completionCondition determines what {@link PointOfInterestType}s to find when searching using this one
+	 *                            {@link PointOfInterestType#UNEMPLOYED} uses this to find villager workstations
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
+	 * @param blocks all blocks where a {@link PointOfInterest} of this type will be present, will apply to all of the {@link Block}'s {@link BlockState}
+	 * @return a new {@link PointOfInterestType}
 	 */
 	public static PointOfInterestType register(Identifier id, int ticketCount, Predicate<PointOfInterestType> completionCondition, int searchDistance, Block... blocks) {
 		final ImmutableSet.Builder<BlockState> builder = ImmutableSet.builder();
@@ -74,49 +75,113 @@ public final class PointOfInterestHelper {
 			builder.addAll(block.getStateManager().getStates());
 		}
 
-		return register(id, ticketCount, completionCondition, searchDistance, builder.build());
+		return register(id, create(id, ticketCount, completionCondition, searchDistance, builder.build()));
 	}
 
 	/**
 	 * Creates and registers a {@link PointOfInterestType}.
 	 *
-	 * @param id the id of this {@link PointOfInterestType}.
-	 * @param ticketCount the amount of tickets.
-	 * @param searchDistance the search distance.
-	 * @param blocks all {@link BlockState block states} where a {@link PointOfInterest} of this type will be present
-	 * @return a new {@link PointOfInterestType}.
-	 */
-	public static PointOfInterestType register(Identifier id, int ticketCount, int searchDistance, Iterable<BlockState> blocks) {
-		final ImmutableSet.Builder<BlockState> builder = ImmutableSet.builder();
-
-		return register(id, ticketCount, searchDistance, builder.addAll(blocks).build());
-	}
-
-	/**
-	 * Creates and registers a {@link PointOfInterestType}.
-	 *
-	 * @param id the id of this {@link PointOfInterestType}.
-	 * @param ticketCount the amount of tickets.
-	 * @param typePredicate a {@link Predicate} which determines if two {@link PointOfInterestType}s are the same.
-	 * @param searchDistance the search distance.
+	 * @param id the id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
 	 * @param states all {@link BlockState block states} where a {@link PointOfInterest} of this type will be present
-	 * @return a new {@link PointOfInterestType}.
+	 * @return a new {@link PointOfInterestType}
 	 */
-	public static PointOfInterestType register(Identifier id, int ticketCount, Predicate<PointOfInterestType> typePredicate, int searchDistance, Iterable<BlockState> states) {
+	public static PointOfInterestType register(Identifier id, int ticketCount, int searchDistance, Iterable<BlockState> states) {
 		final ImmutableSet.Builder<BlockState> builder = ImmutableSet.builder();
 
-		return register(id, ticketCount, typePredicate, searchDistance, builder.addAll(states).build());
+		return register(id, create(id, ticketCount, searchDistance, builder.addAll(states).build()));
 	}
 
-	// INTERNAL METHODS
+	/**
+	 * Creates and registers a {@link PointOfInterestType}.
+	 *
+	 * @param id the id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param completionCondition determines what {@link PointOfInterestType}s to find when searching using this one
+	 *                            {@link PointOfInterestType#UNEMPLOYED} uses this to find villager workstations
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
+	 * @param states all {@link BlockState block states} where a {@link PointOfInterest} of this type will be present
+	 * @return a new {@link PointOfInterestType}
+	 */
+	public static PointOfInterestType register(Identifier id, int ticketCount, Predicate<PointOfInterestType> completionCondition, int searchDistance, Iterable<BlockState> states) {
+		final ImmutableSet.Builder<BlockState> builder = ImmutableSet.builder();
 
-	private static PointOfInterestType register(Identifier id, int ticketCount, int searchDistance, Set<BlockState> states) {
-		return Registry.register(Registry.POINT_OF_INTEREST_TYPE, id, PointOfInterestTypeAccessor.callSetup(
-				PointOfInterestTypeAccessor.callCreate(id.toString(), states, ticketCount, searchDistance)));
+		return register(id, create(id, ticketCount, completionCondition, searchDistance, builder.addAll(states).build()));
 	}
 
-	private static PointOfInterestType register(Identifier id, int ticketCount, Predicate<PointOfInterestType> typePredicate, int searchDistance, Set<BlockState> states) {
-		return Registry.register(Registry.POINT_OF_INTEREST_TYPE, id, PointOfInterestTypeAccessor.callSetup(
-				PointOfInterestTypeAccessor.callCreate(id.toString(), states, ticketCount, typePredicate, searchDistance)));
+	/**
+	 * Creates and registers a {@link PointOfInterestType}.
+	 *
+	 * @param id the id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
+	 * @param states all {@link BlockState block states} where a {@link PointOfInterest} of this type will be present
+	 * @return a new {@link PointOfInterestType}
+	 */
+	public static PointOfInterestType register(Identifier id, int ticketCount, int searchDistance, Set<BlockState> states) {
+		return register(id, create(id, ticketCount, searchDistance, ImmutableSet.copyOf(states)));
+	}
+
+	/**
+	 * Creates and registers a {@link PointOfInterestType}.
+	 *
+	 * @param id the id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param completionCondition determines what {@link PointOfInterestType}s to find when searching using this one
+	 *                            {@link PointOfInterestType#UNEMPLOYED} uses this to find villager workstations
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
+	 * @param states all {@link BlockState block states} where a {@link PointOfInterest} of this type will be present
+	 * @return a new {@link PointOfInterestType}
+	 */
+	public static PointOfInterestType register(Identifier id, int ticketCount, Predicate<PointOfInterestType> completionCondition, int searchDistance, Set<BlockState> states) {
+		return register(id, create(id, ticketCount, completionCondition, searchDistance, ImmutableSet.copyOf(states)));
+	}
+
+	/**
+	 * Registers a {@link PointOfInterestType}.
+	 *
+	 * @param id the {@link Identifier} to register this {@link PointOfInterestType} under
+	 * @param poiType the {@link PointOfInterestType} to register
+	 * @return the given {@link PointOfInterestType}
+	 */
+	public static PointOfInterestType register(Identifier id, PointOfInterestType poiType) {
+		return Registry.register(Registry.POINT_OF_INTEREST_TYPE, id, poiType);
+	}
+
+	/**
+	 * Creates a {@link PointOfInterestType}.
+	 *
+	 * <p>You usually want to register a {@link PointOfInterestType} instead.</p>
+	 *
+	 * @param id the id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
+	 * @param states all {@link BlockState block states} where a {@link PointOfInterest} of this type will be present
+	 * @return a new {@link PointOfInterestType}
+	 * @see PointOfInterestHelper#register(Identifier, int, int, Set)
+	 */
+	public static PointOfInterestType create(Identifier id, int ticketCount, int searchDistance, Set<BlockState> states) {
+		return PointOfInterestTypeAccessor.callSetup(
+				PointOfInterestTypeAccessor.callCreate(id.toString(), states, ticketCount, searchDistance));
+	}
+
+	/**
+	 * Creates a {@link PointOfInterestType}.
+	 *
+	 * <p>You usually want to register a {@link PointOfInterestType} instead.</p>
+	 *
+	 * @param id the id of this {@link PointOfInterestType}
+	 * @param ticketCount the max amount of tickets available to be checked out from searches
+	 * @param completionCondition determines what {@link PointOfInterestType}s to find when searching using this one
+	 *                            {@link PointOfInterestType#UNEMPLOYED} uses this to find villager workstations
+	 * @param searchDistance the distance in blocks from the {@link PointOfInterest} a {@link net.minecraft.entity.mob.MobEntity} can be before considering it reached
+	 * @param states all {@link BlockState block states} where a {@link PointOfInterest} of this type will be present
+	 * @return a new {@link PointOfInterestType}
+	 * @see PointOfInterestHelper#register(Identifier, int, Predicate, int, Set)
+	 */
+	public static PointOfInterestType create(Identifier id, int ticketCount, Predicate<PointOfInterestType> completionCondition, int searchDistance, Set<BlockState> states) {
+		return PointOfInterestTypeAccessor.callSetup(
+				PointOfInterestTypeAccessor.callCreate(id.toString(), states, ticketCount, completionCondition, searchDistance));
 	}
 }
