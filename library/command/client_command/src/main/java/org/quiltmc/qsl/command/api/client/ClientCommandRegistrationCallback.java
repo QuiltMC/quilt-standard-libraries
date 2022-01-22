f43e4c551443cc7c3e75f5c378040a5263b9b63a
William Bradford Larcombe <william@williambl.com>
2022-01-22 14:49:56 +0000

    Command module (#33)
    * Add commands module
    
    * Add CommandRegistrationCallback
    
    * Change CommandRegistrationCallback arguments
    RegistrationEnvironment's fields are package-private :|
    
    * Add client commands
    
    * javadoc
    
    * removed "registerForIntegrated"
    
    * silence "unchecked Optional.get()" warning
    
    * use static import for CommandManager.literal
    
    * optimize imports
    
    * Revert "removed "registerForIntegrated""
    
    This reverts commit 45ca529a418c1fe3ef1f901c0386eca1e31e980a.
    
    * simplify identifiers
    
    * Add more javadoc
    
    * simplify if statement
    
    * fix indentation
    
    * Make server-sided commands have precedence over client-sided ones.
    
    * Cleanup
    
    * Rename qsl-commands -> qsl_commands
    
    * Rename command module + library
    
    * Improve command documentation, mark ClientCommandInternals as internal
    
    * Fix command api requiring wrong loader
    
    * Fix command module name
    
    * Apply licenses for command module
    
    * Fix command api test mod on dedi server
    
    * Cleanup command code and documentation
    
    * Add get/set meta in quilt client command source
    
    * ArrayEvent -> Event
    
    * Split client command api into its own module
    
    * Apply licenses
    
    * Shut up a warning
    
    * Fix library name commands -> command
    
    Co-authored-by: TehcJS <tehcdev@gmail.com>

---
+++ library/command/client_command/src/main/java/org/quiltmc/qsl/command/api/client/ClientCommandRegistrationCallback.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.command.api.client;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.quiltmc.qsl.base.api.event.Event;
+
+/**
+ * Callback for registering client-side commands.
+ *
+ * @see ClientCommandManager
+ */
+@FunctionalInterface
+@Environment(EnvType.CLIENT)
+public interface ClientCommandRegistrationCallback {
+	/**
+	 * Event invoked when client-sided commands are registered.
+	 */
+	Event<ClientCommandRegistrationCallback> EVENT = Event.create(ClientCommandRegistrationCallback.class, callbacks -> dispatcher -> {
+		for (var callback : callbacks) {
+			callback.registerCommands(dispatcher);
+		}
+	});
+
+	/**
+	 * Called when client-side commands are registered.
+	 *
+	 * @param dispatcher the command dispatcher.
+	 */
+	void registerCommands(CommandDispatcher<QuiltClientCommandSource> dispatcher);
+}
