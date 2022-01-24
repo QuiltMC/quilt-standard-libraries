3a56be19f4b376d8d19d9afc7eca06002d2dcb3e
Glitch <glitch.g3431@gmail.com>
2022-01-24 14:37:46 -0600

    Apply suggestions from code review

--- library/gui/tooltip/src/main/java/org/quiltmc/qsl/tooltip/api/package-info.java
+++ library/gui/tooltip/src/main/java/org/quiltmc/qsl/tooltip/api/package-info.java
@@ -7,8 +7,8 @@
  * <p>
  * Those tooltip components are created using mainly two classes:
  * <ul>
- *     <li>{@link net.minecraft.client.item.TooltipData} which will hold information about the tooltip, exists on both sides;</li>
- *     <li>{@link net.minecraft.client.gui.tooltip.TooltipComponent} which will do the rendering of the tooltip, only exists on the client.</li>
+ *     <li>{@link net.minecraft.client.item.TooltipData}, which holds information about the tooltip, exists on both sides;</li>
+ *     <li>{@link net.minecraft.client.gui.tooltip.TooltipComponent}, which does the rendering of the tooltip, only exists on the client.</li>
  * </ul>
  * An item can return a custom tooltip data by overriding the method {@link net.minecraft.item.Item#getTooltipData(net.minecraft.item.ItemStack)}.
  * However, there is no method in Vanilla to convert custom tooltip data into a component.
