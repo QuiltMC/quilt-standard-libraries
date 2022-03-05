a4cb8211a185fe89e9da836bdb2fec39ab3e48d9
FoundationGames <43485105+FoundationGames@users.noreply.github.com>
2022-03-05 09:46:24 -0800

    Registry Module/Events (#38)
    * Add basic registry entry add event
    
    * Refactor tests, add registry monitor (a more advanced alternative to the registry event)
    
    * Move packages to match the organization of other modules, add the ability to chain filters for registry monitors
    
    * Use "this." to clarify some method calls in RegistryMonitor
    
    Co-authored-by: LambdAurora <aurora42lambda@gmail.com>
    
    * Very experimental changes in order to ideally speed up registry iteration, potentially assisting with monitoring larger registries
    
    * Document classes
    
    * Clean up docs, prefix accessor method in SimpleRegistryAccessor
    
    * Follow Javadoc conventions
    
    * Mark impl classes as ApiStatus.Internal, use inline return JD tags for getter methods
    
    * Fix doc and update to new module naming
    
    * Update to 1.18, add icon
    
    * Tweak and update to 22w06a.
    
    * Improve RegistryMonitor test.
    
    * Update library/core/registry/src/testmod/java/org/quiltmc/qsl/registry/test/RegistryLibMonitorTest.java
    
    Co-authored-by: Eli Orona <eliorona@live.com>
    
    * Improve javadoc and change injection point for registry event storage.
    
    * Update library/core/registry/src/main/java/org/quiltmc/qsl/registry/api/event/RegistryEntryContext.java
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * Improve documentation.
    
    Co-authored-by: LambdAurora <aurora42lambda@gmail.com>
    Co-authored-by: LambdAurora <email@lambdaurora.dev>
    Co-authored-by: Eli Orona <eliorona@live.com>
    Co-authored-by: Glitch <glitch.g3431@gmail.com>

---
+++ library/core/registry/src/main/java/org/quiltmc/qsl/registry/mixin/SimpleRegistryMixin.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.registry.mixin;
+
+import com.mojang.serialization.Lifecycle;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.util.Holder;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.util.registry.SimpleRegistry;
+
+import org.quiltmc.qsl.registry.impl.event.MutableRegistryEntryContextImpl;
+import org.quiltmc.qsl.registry.impl.event.RegistryEventStorage;
+
+/**
+ * Stores and invokes registry events.
+ */
+@Mixin(SimpleRegistry.class)
+public abstract class SimpleRegistryMixin<V> extends Registry<V> {
+	@Unique
+	private final MutableRegistryEntryContextImpl<V> quilt$entryContext = new MutableRegistryEntryContextImpl<>(this);
+
+	protected SimpleRegistryMixin(RegistryKey<? extends Registry<V>> key, Lifecycle lifecycle) {
+		super(key, lifecycle);
+	}
+
+	/**
+	 * Invokes the entry add event.
+	 */
+	@Inject(
+			method = "method_31051(ILnet/minecraft/util/registry/RegistryKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;Z)Lnet/minecraft/util/Holder;",
+			at = @At("RETURN")
+	)
+	private void quilt$invokeEntryAddEvent(int rawId, RegistryKey<V> key, V entry, Lifecycle lifecycle, boolean checkDuplicateKeys,
+	                                       CallbackInfoReturnable<Holder<V>> cir) {
+		this.quilt$entryContext.set(key.getValue(), entry, rawId);
+		RegistryEventStorage.as(this).quilt$getEntryAddedEvent().invoker().onAdded(this.quilt$entryContext);
+	}
+}
