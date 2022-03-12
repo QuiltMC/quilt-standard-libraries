ffe4a394589509afac487c488ad32c432ee44841
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-03-12 18:04:48 +0200

    DispatchedDictTest: move inner classes to their own files

--- library/data/registry_dicts/src/testmod/java/org/quiltmc/qsl/registry/dict/test/DispatchedDictTest.java
+++ library/data/registry_dicts/src/testmod/java/org/quiltmc/qsl/registry/dict/test/DispatchedDictTest.java
@@ -16,11 +16,6 @@
 
 package org.quiltmc.qsl.registry.dict.test;
 
-import java.util.Map;
-
-import com.google.common.collect.ImmutableMap;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.entity.player.PlayerEntity;
@@ -28,76 +23,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.util.*;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
-import org.quiltmc.qsl.registry.dict.api.DispatchedType;
 import org.quiltmc.qsl.registry.dict.api.RegistryDict;
 import org.quiltmc.qsl.registry.dict.api.RegistryExtensions;
 
 public class DispatchedDictTest implements ModInitializer {
-	public static abstract class FuncValue implements DispatchedType {
-		// in a real-world application, you'd probably use a Registry for this
-		public static final Map<Identifier, Codec<? extends FuncValue>> CODECS = Util.make(() ->
-				ImmutableMap.<Identifier, Codec<? extends FuncValue>>builder()
-						.put(SendMessageFuncValue.TYPE, SendMessageFuncValue.CODEC)
-						.put(GiveStackFuncValue.TYPE, GiveStackFuncValue.CODEC)
-						.build());
-
-		protected final Identifier type;
-
-		protected FuncValue(Identifier type) {
-			this.type = type;
-		}
-
-		@Override
-		public final Identifier getType() {
-			return type;
-		}
-
-		public abstract void invoke(ServerPlayerEntity player);
-	}
-
-	public static final class SendMessageFuncValue extends FuncValue {
-		public static final Identifier TYPE = new Identifier("quilt", "send_message");
-		public static final Codec<SendMessageFuncValue> CODEC = RecordCodecBuilder.create(instance ->
-				instance.group(Codec.STRING.fieldOf("message").forGetter(sm -> sm.message))
-						.apply(instance, SendMessageFuncValue::new));
-
-		private final String message;
-
-		public SendMessageFuncValue(String message) {
-			super(TYPE);
-			this.message = message;
-		}
-
-		@Override
-		public void invoke(ServerPlayerEntity player) {
-			player.sendMessage(Text.of("Quilt says: '" + message + "'"), false);
-		}
-	}
-
-	public static final class GiveStackFuncValue extends FuncValue {
-		public static final Identifier TYPE = new Identifier("quilt", "give_stack");
-		public static final Codec<GiveStackFuncValue> CODEC = RecordCodecBuilder.create(instance ->
-				instance.group(ItemStack.CODEC.fieldOf("stack").forGetter(gs -> gs.stack))
-						.apply(instance, GiveStackFuncValue::new));
-
-		private final ItemStack stack;
-
-		public GiveStackFuncValue(ItemStack stack) {
-			super(TYPE);
-			this.stack = stack;
-		}
-
-		@Override
-		public void invoke(ServerPlayerEntity player) {
-			player.getInventory().offerOrDrop(stack.copy());
-		}
-	}
-
 	public static final RegistryDict<Item, FuncValue> MODULAR_FUNCTION =
 			RegistryDict.dispatchedBuilder(Registry.ITEM, new Identifier("quilt", "modular_function"),
 					FuncValue.class, FuncValue.CODECS::get).build();
