bc22d62550fa07cb912592a93e2ae640caef811d
Noah van der Aa <ndvdaa@gmail.com>
2022-03-25 23:56:44 +0100

    add newline after license...

--- library/worldgen/dimension/src/testmod/java/org/quiltmc/qsl/worldgen/dimension/EmptyChunkGenerator.java
+++ library/worldgen/dimension/src/testmod/java/org/quiltmc/qsl/worldgen/dimension/EmptyChunkGenerator.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.quiltmc.qsl.worldgen.dimension;
 
 import com.mojang.serialization.Codec;
