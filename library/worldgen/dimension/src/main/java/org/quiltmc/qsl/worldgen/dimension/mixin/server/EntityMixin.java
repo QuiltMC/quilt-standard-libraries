64a6025ab1c898150079f9a2b39d8d8847607433
Noah van der Aa <ndvdaa@gmail.com>
2022-03-26 11:18:17 +0100

    Address comments

--- library/worldgen/dimension/src/main/java/org/quiltmc/qsl/worldgen/dimension/mixin/server/EntityMixin.java
+++ library/worldgen/dimension/src/main/java/org/quiltmc/qsl/worldgen/dimension/mixin/server/EntityMixin.java
@@ -21,6 +21,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.TeleportTarget;
 import org.quiltmc.qsl.worldgen.dimension.access.EntityAccess;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -28,18 +29,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public class EntityMixin implements EntityAccess {
 
-	public TeleportTarget overriddenTeleportTarget = null;
+	@Unique
+	public TeleportTarget quilt$overriddenTeleportTarget = null;
 
 	@Inject(method = "getTeleportTarget(Lnet/minecraft/server/world/ServerWorld;)Lnet/minecraft/world/TeleportTarget;", at = @At("HEAD"), cancellable = true)
 	public void getOverriddenTeleportTarget(ServerWorld destination, CallbackInfoReturnable<TeleportTarget> cir) {
-		if (overriddenTeleportTarget != null) {
-			cir.setReturnValue(overriddenTeleportTarget);
+		if (quilt$overriddenTeleportTarget != null) {
+			cir.setReturnValue(quilt$overriddenTeleportTarget);
 		}
 	}
 
 	@Override
 	public void setTeleportTarget(TeleportTarget target) {
-		overriddenTeleportTarget = target;
+		quilt$overriddenTeleportTarget = target;
 	}
 
 }
