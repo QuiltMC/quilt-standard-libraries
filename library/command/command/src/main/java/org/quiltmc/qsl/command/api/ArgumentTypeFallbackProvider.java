773b3affe21e4b9f52d8dea1dcf7affa813e1a50
ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
2022-04-05 17:35:10 +0300

    Command module: add server-side argument types (#68)
    * Command module: add server-side argument types
    
    Basically an almost-direct port of the Colonel API (https://gitlab.com/stellardrift/colonel), minus syncing stuff (which requires networking, and such depends on QuiltMC#34).
    
    * Add Colonel licenses to all files sourced from Colonel
    
    * Add enum argument type
    
    This functions as a test of the ServerArgumentType API, and is also pretty convenient, IMO
    The serializer leaves much to be desired, though...
    
    * EnumArgumentType: rewrite to make less horrible
    
    * EnumArgumentType: add translation for "unknown value" exception
    
    * EnumArgumentType: add sanity checks
    
    * Implement known argument type syncing
    
    * license
    
    also add missing ApiStatus.Internal annotations
    
    * ID -> identifier
    
    Co-authored-by: LambdAurora <aurora42lambda@gmail.com>
    
    * EnumArgumentType: add javadoc
    
    add EnumArgumentType.getEnum to match other ArgumentType classes
    
    * ServerArgumentTypeImpl: use spaces for record definition indentation
    
    there, now it doesn't look godawful on GitHub
    
    * Move PreLaunchHacks, add server arg testmod, fix up some issues.
    
    * EnumArgumentType: unclunkify vanilla support notice
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * CommandManagerMixin: add details to Colonel section
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * PreLaunchInitializer: add note to remove once QLoader is targeted
    
    * Initializers: log networking support status
    
    * EnumArgumentType: getEnumConstant: verify enum class
    
    enumConstant: cache returned arg types
    
    * ServerArgumentTypes: don't resend commands unless known argument types have changed
    
    * [NOT WORKING] Sync known argument types earlier to avoid resending command tree
    
    ...yeah I somehow broke it
    I have no idea how or why, but Vanilla clients can't connect now because they end up parsing a bad identifier somewhere
    
    KnownArgTypesSync: Vanilla clients now get empty set instead of set being null
    
    * [IT WORKS NOW] restore FMJ changes
    
    this indirectly caused Vanilla clients to not be able to connect...
    because when Minecraft fails when serializing an argument type, *it sends an empty identifier*, which the client promptly chokes on.
    (MC fails to serialize an argument type if it's not registered - and the code that registers EnumArgumentType wasn't running because
    I accidentally ran over the FMJ file.)
    
    Great job, Mojang.
    
    * Fix getEnumConstant.
    
    * Reformat ServerArgumentTypeImpl
    
    Co-authored-by: William Bradford Larcombe <william@williambl.com>
    
    * Make local capture fail hard
    
    Co-authored-by: LambdAurora <email@lambdaurora.dev>
    Co-authored-by: LambdAurora <aurora42lambda@gmail.com>
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    Co-authored-by: William Bradford Larcombe <william@williambl.com>

---
+++ library/command/command/src/main/java/org/quiltmc/qsl/command/api/ArgumentTypeFallbackProvider.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.command.api;
+
+import com.mojang.brigadier.arguments.ArgumentType;
+
+/**
+ * A function that transforms an argument of a custom type into one understandable by a vanilla client.
+ *
+ * @param <T> the original argument type
+ */
+@FunctionalInterface
+public interface ArgumentTypeFallbackProvider<T extends ArgumentType<?>> {
+	/**
+	 * Creates a fallback argument understandable by the vanilla client based on the specified argument.
+	 *
+	 * @param originalArg the original argument
+	 * @return the new vanilla-compatible argument
+	 */
+	ArgumentType<?> createVanillaFallback(T originalArg);
+}
