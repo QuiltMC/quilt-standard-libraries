ae14127e83b1a060c08e1ad447a46a6ce8abd046
LambdAurora <email@lambdaurora.dev>
2022-04-19 16:19:45 +0200

    Recipe API (#77)
    * Recipe API.
    
    * Add package-info.
    
    * Reword last sentence in package-info.
    
    * Update library/data/recipe/src/main/java/org/quiltmc/qsl/recipe/impl/RecipeManagerImpl.java
    
    Co-authored-by: ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Eli Orona <eliorona@live.com>
    
    * Split up big classes, move a bit more to implementation, fix a lot of wording.
    
    * Apply suggestions from code review
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * Clarify javadocs and fix some issues.
    
    * Update library/data/recipe/src/main/java/org/quiltmc/qsl/recipe/api/RecipeManagerHelper.java
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * Add missing QuiltRecipeSerializer implementations and add warning in dev env.
    
    * Port to Quilt Loader stuff.
    
    Co-authored-by: ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
    Co-authored-by: Eli Orona <eliorona@live.com>
    Co-authored-by: Glitch <glitch.g3431@gmail.com>

---
+++ library/data/recipe/src/main/java/org/quiltmc/qsl/recipe/impl/RecipeManagerImpl.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.recipe.impl;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.Streams;
+import com.google.gson.stream.JsonWriter;
+import com.mojang.logging.LogUtils;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.fabricmc.loader.api.FabricLoader;
+import org.jetbrains.annotations.ApiStatus;
+import org.slf4j.Logger;
+
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import org.quiltmc.qsl.recipe.api.RecipeLoadingEvents;
+import org.quiltmc.qsl.recipe.api.serializer.QuiltRecipeSerializer;
+
+@ApiStatus.Internal
+public final class RecipeManagerImpl {
+	/**
+	 * Stores the static recipes which are added to the {@link net.minecraft.recipe.RecipeManager} when recipes are
+	 * loaded.
+	 */
+	private static final Map<Identifier, Recipe<?>> STATIC_RECIPES = new Object2ObjectOpenHashMap<>();
+	static final boolean DEBUG_MODE = Boolean.getBoolean("quilt.recipe.debug");
+	private static final boolean DUMP_MODE = Boolean.getBoolean("quilt.recipe.dump");
+	static final Logger LOGGER = LogUtils.getLogger();
+
+	private RecipeManagerImpl() {
+		throw new UnsupportedOperationException("RecipeManagerImpl only contains static definitions.");
+	}
+
+	public static void registerStaticRecipe(Recipe<?> recipe) {
+		if (STATIC_RECIPES.putIfAbsent(recipe.getId(), recipe) != null) {
+			throw new IllegalArgumentException("Cannot register " + recipe.getId()
+					+ " as another recipe with the same identifier already exists.");
+		}
+	}
+
+	public static void apply(Map<Identifier, JsonElement> map,
+	                         Map<RecipeType<?>, ImmutableMap.Builder<Identifier, Recipe<?>>> builderMap) {
+		var handler = new RegisterRecipeHandlerImpl(map, builderMap);
+		RecipeLoadingEvents.ADD.invoker().addRecipes(handler);
+		STATIC_RECIPES.values().forEach(handler::register);
+		LOGGER.info("Registered {} custom recipes.", handler.registered);
+	}
+
+	public static void applyModifications(RecipeManager recipeManager, Map<RecipeType<?>, Map<Identifier, Recipe<?>>> recipes) {
+		var handler = new ModifyRecipeHandlerImpl(recipeManager, recipes);
+		RecipeLoadingEvents.MODIFY.invoker().modifyRecipes(handler);
+		LOGGER.info("Modified {} recipes.", handler.counter);
+
+		var removeHandler = new RemoveRecipeHandlerImpl(recipeManager, recipes);
+		RecipeLoadingEvents.REMOVE.invoker().removeRecipes(removeHandler);
+		LOGGER.info("Removed {} recipes.", removeHandler.counter);
+
+		if (DUMP_MODE) {
+			dump(recipes.values());
+		}
+
+		if (DEBUG_MODE || FabricLoader.getInstance().isDevelopmentEnvironment()) {
+			for (var serializerEntry : Registry.RECIPE_SERIALIZER.getEntries()) {
+				if (!(serializerEntry.getValue() instanceof QuiltRecipeSerializer)) {
+					LOGGER.warn(
+							"Recipe serializer {} doesn't implement QuiltRecipeSerializer. For full compatibility, the interface should be implemented.",
+							serializerEntry.getKey().getValue()
+					);
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void dump(Collection<Map<Identifier, Recipe<?>>> recipes) {
+		Path debugPath = Paths.get("debug", "quilt", "recipe").normalize();
+
+		if (!Files.exists(debugPath)) {
+			try {
+				Files.createDirectories(debugPath);
+			} catch (IOException e) {
+				LOGGER.error("Failed to create debug directory for recipe dumping.", e);
+				return;
+			}
+		}
+
+		for (var map : recipes) {
+			for (Recipe<?> recipe : map.values()) {
+				if (!(recipe.getSerializer() instanceof QuiltRecipeSerializer)) continue;
+
+				var serializer = (QuiltRecipeSerializer<Recipe<?>>) recipe.getSerializer();
+				JsonObject serialized = serializer.toJson(recipe);
+
+				Path path = debugPath.resolve(recipe.getId().getNamespace() + "/recipes/" + recipe.getId().getPath() + ".json");
+				Path parent = path.getParent();
+
+				if (!Files.exists(parent)) {
+					try {
+						Files.createDirectories(parent);
+					} catch (IOException e) {
+						LOGGER.error("Failed to create parent recipe directory {}. Cannot dump recipe {}.",
+								parent, recipe.getId(), e);
+						continue;
+					}
+				}
+
+				var stringWriter = new StringWriter();
+				var jsonWriter = new JsonWriter(stringWriter);
+				jsonWriter.setLenient(true);
+				jsonWriter.setIndent("  ");
+
+				try {
+					Streams.write(serialized, jsonWriter);
+					Files.writeString(path, stringWriter.toString(),
+							StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+				} catch (IOException e) {
+					LOGGER.error("Failed to write JSON for recipe {}.", recipe.getId(), e);
+				} finally {
+					try {
+						jsonWriter.close();
+					} catch (IOException e) {
+						LOGGER.error("Failed to close JSON writer for recipe {}.", recipe.getId(), e);
+					}
+				}
+			}
+		}
+	}
+}
