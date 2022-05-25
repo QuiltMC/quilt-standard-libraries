64d839ca2fa6b1970ae1aa100b022b34bc3dac7b
Glitch <glitch.g3431@gmail.com>
2022-05-24 20:08:26 -0500

    Create a fat, remapped testmod jar (#114)
    Remapped testmod jars that you can throw into your favorite launcher are created on the module, library, and root levels.
    They are not published to maven, but can be downloaded through Github Actions

--- build-logic/src/main/java/qsl/internal/json/QmjBuilder.java
+++ build-logic/src/main/java/qsl/internal/json/QmjBuilder.java
@@ -34,8 +34,10 @@ public final class QmjBuilder {
 				.endObject() // contact -> metadata
 				.name("license").value("Apache-2.0")
 				.name("icon").value("assets/" + ext.getId().get() + "/icon.png")
-				.endObject(); // metadata -> quilt_loader
-		writer.name("intermediate_mappings").value("net.fabricmc:intermediary");
+				.endObject() // metadata -> quilt_loader
+				.name("intermediate_mappings").value("net.fabricmc:intermediary")
+				.name("load_type").value("if_possible");
+
 		writer.name("depends").beginArray();
 		writer.beginObject()
 				.name("id").value("quilt_loader")
