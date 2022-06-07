8937cfb331d2a0bfe2e80aff95f053be272a7127
LambdAurora <email@lambdaurora.dev>
2022-06-07 17:15:09 +0200

    Fix release workflow misfiling the QSL version field.

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -17,7 +17,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "1.1.0-beta.17";
+	public static final String QSL_VERSION = "1.1.0-beta.18";
 
 	/**
 	 * The target Minecraft version.
