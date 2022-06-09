17b5fa9e5bfe97e863407c447b8b87971874ca6c
LambdAurora <email@lambdaurora.dev>
2022-06-09 02:49:44 +0200

    Fix loading world generation objects from mods via JSON. (#125)
    * Fix loading world generation objects from mods via JSON, fix JSON dimensions not loading at world creation.
    
    * Fix wording.

---
+++ library/core/resource_loader/src/main/java/org/quiltmc/qsl/resource/loader/mixin/TestServerMixin.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.resource.loader.mixin;
+
+import java.util.function.Supplier;
+
+import com.mojang.serialization.JsonOps;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.pack.DataPackSettings;
+import net.minecraft.test.TestServer;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.RegistryOps;
+
+import org.quiltmc.qsl.resource.loader.impl.ModResourcePackUtil;
+
+@Mixin(TestServer.class)
+public class TestServerMixin {
+	@ModifyArg(
+			method = "create",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/unmapped/C_kjxfcecs$C_nrmvgbka;<init>(Lnet/minecraft/resource/pack/ResourcePackManager;Lnet/minecraft/resource/pack/DataPackSettings;Z)V"
+			),
+			index = 1
+	)
+	private static DataPackSettings replaceDefaultDataPackSettings(DataPackSettings initialDataPacks) {
+		return ModResourcePackUtil.DEFAULT_SETTINGS;
+	}
+
+	@Redirect(method = "method_40377", at = @At(value = "INVOKE", target = "Ljava/util/function/Supplier;get()Ljava/lang/Object;"))
+	private static Object loadRegistry(Supplier<Object> unused, ResourceManager resourceManager) {
+		DynamicRegistryManager.Writable registryManager = DynamicRegistryManager.builtInCopy();
+		// Force-loads the dynamic registry from data-packs as some mods may define dynamic game objects via data-driven capabilities.
+		RegistryOps.createAndLoad(JsonOps.INSTANCE, registryManager, resourceManager);
+		return registryManager.freeze();
+	}
+}
--- library/core/resource_loader/src/main/java/org/quiltmc/qsl/resource/loader/mixin/server/MainMixin.java
+++ library/core/resource_loader/src/main/java/org/quiltmc/qsl/resource/loader/mixin/server/MainMixin.java
@@ -16,20 +16,63 @@
 
 package org.quiltmc.qsl.resource.loader.mixin.server;
 
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.Lifecycle;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.pack.DataPackSettings;
 import net.minecraft.server.Main;
 import net.minecraft.server.WorldStem;
+import net.minecraft.server.dedicated.ServerPropertiesLoader;
+import net.minecraft.util.Util;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.SaveProperties;
+import net.minecraft.world.gen.GeneratorOptions;
+import net.minecraft.world.level.LevelInfo;
+import net.minecraft.world.level.LevelProperties;
+import net.minecraft.world.level.storage.LevelStorage;
 
 import org.quiltmc.qsl.resource.loader.api.ResourceLoaderEvents;
+import org.quiltmc.qsl.resource.loader.impl.DataPackLoadingContext;
+import org.quiltmc.qsl.resource.loader.impl.ModResourcePackUtil;
 
 @Mixin(Main.class)
 public class MainMixin {
+	@Shadow
+	@Final
+	private static Logger LOGGER;
+
+	@ModifyArg(
+			method = "main",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/Objects;requireNonNullElse(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+					remap = true
+			),
+			index = 1,
+			remap = false
+	)
+	private static Object replaceDefaultDataPackSettings(Object base) {
+		return ModResourcePackUtil.DEFAULT_SETTINGS;
+	}
+
 	@Inject(
 			method = "main",
 			at = @At(
@@ -61,5 +104,42 @@ public class MainMixin {
 	private static Throwable onFailedReloadResources(Throwable exception) {
 		ResourceLoaderEvents.END_DATA_PACK_RELOAD.invoker().onEndDataPackReload(null, null, exception);
 		return exception; // noop
+	}
+
+	@Inject(
+			method = "method_43613",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/level/LevelProperties;<init>(Lnet/minecraft/world/level/LevelInfo;Lnet/minecraft/world/gen/GeneratorOptions;Lcom/mojang/serialization/Lifecycle;)V"
+			),
+			cancellable = true,
+			locals = LocalCapture.CAPTURE_FAILHARD
+	)
+	private static void onLoadDataPacks(LevelStorage.Session session, OptionSet optionSet, OptionSpec optionSpec,
+			ServerPropertiesLoader serverPropertiesLoader, OptionSpec optionSpec2,
+			ResourceManager resourceManager, DataPackSettings dataPackSettings,
+			CallbackInfoReturnable<Pair<SaveProperties, DynamicRegistryManager.Frozen>> cir,
+			DynamicRegistryManager.Writable writable,
+			DynamicOps<NbtElement> nbtRegistryOps,
+			SaveProperties saveProperties,
+			LevelInfo levelInfo,
+			GeneratorOptions existingGeneratorOptions) {
+		var dataPackLoadingContext = new DataPackLoadingContext(writable, resourceManager);
+		DataResult<GeneratorOptions> result = dataPackLoadingContext.loadGeneratorOptions(existingGeneratorOptions, dataPackLoadingContext.loadRegistries());
+
+		DynamicRegistryManager.Frozen frozenRegistryManager = dataPackLoadingContext.registryManager().freeze();
+		Lifecycle lifecycle = result.lifecycle().add(frozenRegistryManager.allElementsLifecycle());
+		GeneratorOptions generatorOptions = result.getOrThrow(
+				false, Util.addPrefix("Error parsing worldgen settings after loading data-packs: ", LOGGER::error)
+		);
+
+		if (frozenRegistryManager.get(Registry.WORLD_PRESET_WORLDGEN).size() == 0) {
+			throw new IllegalStateException("Needs at least one world preset to continue");
+		} else if (frozenRegistryManager.get(Registry.BIOME_KEY).size() == 0) {
+			throw new IllegalStateException("Needs at least one biome to continue");
+		} else {
+			var levelProperties = new LevelProperties(levelInfo, generatorOptions, lifecycle);
+			cir.setReturnValue(Pair.of(levelProperties, frozenRegistryManager));
+		}
 	}
 }
