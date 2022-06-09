58f805b31dd5ed47626dd07e59b25c4d68e4d208
Eli Orona <eliorona@live.com>
2022-06-08 21:20:15 -0700

    Remove `@Overwrite`s and make values load consistently and debug

---
+++ library/item/item_content_registry/src/main/java/org/quiltmc/qsl/item/content_registry/impl/ItemContentRegistriesInitializer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.item.content_registry.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.resource.loader.api.ResourceLoaderEvents;
+
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.item.Item;
+
+public class ItemContentRegistriesInitializer implements ModInitializer {
+	public static final Map<Item, Integer> FUEL_MAP = new HashMap<>();
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		AbstractFurnaceBlockEntity.createFuelTimeMap();
+		ResourceLoaderEvents.END_DATA_PACK_RELOAD.register((server, resourceManager, error) -> {
+			FUEL_MAP.clear();
+			AbstractFurnaceBlockEntity.createFuelTimeMap();
+		});
+	}
+}
--- library/item/item_content_registry/src/main/java/org/quiltmc/qsl/item/content_registry/mixin/AbstractFurnaceBlockEntityMixin.java
+++ library/item/item_content_registry/src/main/java/org/quiltmc/qsl/item/content_registry/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -19,64 +19,48 @@ package org.quiltmc.qsl.item.content_registry.mixin;
 import java.util.Map;
 
 import org.quiltmc.qsl.item.content_registry.api.ItemContentRegistries;
+import org.quiltmc.qsl.item.content_registry.impl.ItemContentRegistriesInitializer;
+import org.quiltmc.qsl.registry.attachment.impl.RegistryEntryAttachmentHolder;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.SharedConstants;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tag.TagKey;
-import net.minecraft.util.Holder;
-import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
-public class AbstractFurnaceBlockEntityMixin {
-	/**
-	 * @author QSL Item Content Registry
-	 * @reason Not sure.
-	 */
-	@Overwrite
-	private static void addFuel(Map<Item, Integer> fuelTimes, ItemConvertible itemConvertable, int fuelTime) {
-		Item item = itemConvertable.asItem();
-		if (isNonFlammableWood(item)) {
-			if (SharedConstants.isDevelopment) {
-				throw Util.throwOrPause(
-						new IllegalStateException(
-								"A developer tried to explicitly make fire resistant item " + item.getName(null).getString() + " a furnace fuel. That will not work!"
-						)
-				);
-			}
-		} else {
-			ItemContentRegistries.FUEL_TIME.put(item, fuelTime);
+public abstract class AbstractFurnaceBlockEntityMixin {
+	@Inject(method = "createFuelTimeMap", at = @At("HEAD"), cancellable = true)
+	private static void returnCachedMap(CallbackInfoReturnable<Map<Item, Integer>> cir) {
+		if (!ItemContentRegistriesInitializer.FUEL_MAP.isEmpty()) {
+			cir.setReturnValue(ItemContentRegistriesInitializer.FUEL_MAP);
 		}
 	}
 
-	/**
-	 * @author QSL Item Content Registry
-	 * @reason Not sure.
-	 */
-	@Overwrite
-	private static void addFuel(Map<Item, Integer> fuelTimes, TagKey<Item> tagKey, int fuelTime) {
-		for(Holder<Item> holder : Registry.ITEM.getTagOrEmpty(tagKey)) {
-			if (!isNonFlammableWood(holder.value())) {
-				if (SharedConstants.isDevelopment) {
-					throw Util.throwOrPause(
-							new IllegalStateException(
-									"A developer tried to explicitly make fire resistant item " + holder.value().getName(null).getString() + " a furnace fuel. That will not work!"
-							)
-					);
-				}
-			}
-		}
+	@Inject(method = "createFuelTimeMap", at = @At("TAIL"))
+	private static void setCachedMap(CallbackInfoReturnable<Map<Item, Integer>> cir) {
+		var builtin = RegistryEntryAttachmentHolder.getBuiltin(Registry.ITEM);
+		builtin.valueTable.row(ItemContentRegistries.FUEL_TIME).forEach((item, o) -> addFuel(cir.getReturnValue(), item, (Integer) o));
+		builtin.valueTagTable.row(ItemContentRegistries.FUEL_TIME).forEach((tag, o) -> addFuel(cir.getReturnValue(), tag, (Integer) o));
 
-		ItemContentRegistries.FUEL_TIME.put(tagKey, fuelTime);
+		ItemContentRegistriesInitializer.FUEL_MAP.putAll(cir.getReturnValue());
+	}
+
+	@Inject(method = "addFuel(Ljava/util/Map;Lnet/minecraft/item/ItemConvertible;I)V", at = @At("TAIL"))
+	private static void addFuel(Map<Item, Integer> fuelTimes, ItemConvertible item, int fuelTime, CallbackInfo ci) {
+		ItemContentRegistries.FUEL_TIME.put(item.asItem(), fuelTime);
+	}
+
+	@Inject(method = "addFuel(Ljava/util/Map;Lnet/minecraft/tag/TagKey;I)V", at = @At("TAIL"))
+	private static void addTagFuel(Map<Item, Integer> fuelTimes, TagKey<Item> tag, int fuelTime, CallbackInfo ci) {
+		ItemContentRegistries.FUEL_TIME.put(tag, fuelTime);
 	}
 
 
@@ -91,7 +75,12 @@ public class AbstractFurnaceBlockEntityMixin {
 	}
 
 	@Shadow
-	private static boolean isNonFlammableWood(Item item) {
-		throw new AssertionError();
+	private static void addFuel(Map<Item, Integer> fuelTimes, ItemConvertible item, int fuelTime) {
+		throw new AssertionError("Not shadowed");
+	}
+
+	@Shadow
+	private static void addFuel(Map<Item, Integer> fuelTimes, TagKey<Item> tag, int fuelTime) {
+		throw new AssertionError("Not shadowed");
 	}
 }
