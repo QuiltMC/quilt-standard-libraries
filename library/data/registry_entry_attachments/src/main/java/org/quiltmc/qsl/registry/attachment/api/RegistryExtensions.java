910b9a0d98e74e49a4099cbbfdc443b18daa4725
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-06-10 14:42:28 +0300

    Javadoc and general cleanup

--- library/data/registry_entry_attachments/src/main/java/org/quiltmc/qsl/registry/attachment/api/RegistryExtensions.java
+++ library/data/registry_entry_attachments/src/main/java/org/quiltmc/qsl/registry/attachment/api/RegistryExtensions.java
@@ -23,43 +23,112 @@ import net.minecraft.util.registry.Registry;
  * Extensions for working with {@link Registry}s.
  */
 public final class RegistryExtensions {
-	public static <R, T extends R, V1> T register(Registry<R> registry, Identifier id, T toRegister,
+	/**
+	 * Utility method to register an entry and associate a value to it in an attachment.
+	 *
+	 * @param registry target registry
+	 * @param id       entry identifier
+	 * @param entry    entry to register
+	 * @param attach1  attachment
+	 * @param value1   value to associate to entry in attachment
+	 * @param <R>      type of the entries in the registry
+	 * @param <T>      type of the entry we're currently registering (may be a subclass of {@code R})
+	 * @param <V1>     type of the attached value
+	 * @return the newly registered entry
+	 */
+	public static <R, T extends R, V1> T register(Registry<R> registry, Identifier id, T entry,
 			RegistryEntryAttachment<R, V1> attach1, V1 value1) {
-		Registry.register(registry, id, toRegister);
-		attach1.put(toRegister, value1);
-		return toRegister;
+		Registry.register(registry, id, entry);
+		attach1.put(entry, value1);
+		return entry;
 	}
 
-	public static <R, T extends R, V1, V2> T register(Registry<R> registry, Identifier id, T toRegister,
+	/**
+	 * Utility method to register an entry and associate values to it in multiple attachments.
+	 *
+	 * @param registry target registry
+	 * @param id       entry identifier
+	 * @param entry    entry to register
+	 * @param attach1  first attachment
+	 * @param value1   value to associate to entry in first attachment
+	 * @param attach2  second attachment
+	 * @param value2   value to associate to entry in second attachment
+	 * @param <R>      type of the entries in the registry
+	 * @param <T>      type of the entry we're currently registering (may be a subclass of {@code R})
+	 * @param <V1>     type of the first attached value
+	 * @param <V2>     type of the second attached value
+	 * @return the newly registered entry
+	 */
+	public static <R, T extends R, V1, V2> T register(Registry<R> registry, Identifier id, T entry,
 			RegistryEntryAttachment<R, V1> attach1, V1 value1,
 			RegistryEntryAttachment<R, V2> attach2, V2 value2) {
-		Registry.register(registry, id, toRegister);
-		attach1.put(toRegister, value1);
-		attach2.put(toRegister, value2);
-		return toRegister;
+		Registry.register(registry, id, entry);
+		attach1.put(entry, value1);
+		attach2.put(entry, value2);
+		return entry;
 	}
-
-	public static <R, T extends R, V1, V2, V3> T register(Registry<R> registry, Identifier id, T toRegister,
+	/**
+	 * Utility method to register an entry and associate values to it in multiple attachments.
+	 *
+	 * @param registry target registry
+	 * @param id       entry identifier
+	 * @param entry    entry to register
+	 * @param attach1  first attachment
+	 * @param value1   value to associate to entry in first attachment
+	 * @param attach2  second attachment
+	 * @param value2   value to associate to entry in second attachment
+	 * @param attach3  third attachment
+	 * @param value3   value to associate to entry in third attachment
+	 * @param <R>      type of the entries in the registry
+	 * @param <T>      type of the entry we're currently registering (may be a subclass of {@code R})
+	 * @param <V1>     type of the first attached value
+	 * @param <V2>     type of the second attached value
+	 * @param <V3>     type of the third attached value
+	 * @return the newly registered entry
+	 */
+	public static <R, T extends R, V1, V2, V3> T register(Registry<R> registry, Identifier id, T entry,
 			RegistryEntryAttachment<R, V1> attach1, V1 value1,
 			RegistryEntryAttachment<R, V2> attach2, V2 value2,
 			RegistryEntryAttachment<R, V3> attach3, V3 value3) {
-		Registry.register(registry, id, toRegister);
-		attach1.put(toRegister, value1);
-		attach2.put(toRegister, value2);
-		attach3.put(toRegister, value3);
-		return toRegister;
+		Registry.register(registry, id, entry);
+		attach1.put(entry, value1);
+		attach2.put(entry, value2);
+		attach3.put(entry, value3);
+		return entry;
 	}
 
-	public static <R, T extends R, V1, V2, V3, V4> T register(Registry<R> registry, Identifier id, T toRegister,
+	/**
+	 * Utility method to register an entry and associate values to it in multiple attachments.
+	 *
+	 * @param registry target registry
+	 * @param id       entry identifier
+	 * @param entry    entry to register
+	 * @param attach1  first attachment
+	 * @param value1   value to associate to entry in first attachment
+	 * @param attach2  second attachment
+	 * @param value2   value to associate to entry in second attachment
+	 * @param attach3  third attachment
+	 * @param value3   value to associate to entry in third attachment
+	 * @param attach4  fourth attachment
+	 * @param value4   value to associate to entry in fourth attachment
+	 * @param <R>      type of the entries in the registry
+	 * @param <T>      type of the entry we're currently registering (may be a subclass of {@code R})
+	 * @param <V1>     type of the first attached value
+	 * @param <V2>     type of the second attached value
+	 * @param <V3>     type of the third attached value
+	 * @param <V4>     type of the fourth attached value
+	 * @return the newly registered entry
+	 */
+	public static <R, T extends R, V1, V2, V3, V4> T register(Registry<R> registry, Identifier id, T entry,
 			RegistryEntryAttachment<R, V1> attach1, V1 value1,
 			RegistryEntryAttachment<R, V2> attach2, V2 value2,
 			RegistryEntryAttachment<R, V3> attach3, V3 value3,
 			RegistryEntryAttachment<R, V4> attach4, V4 value4) {
-		Registry.register(registry, id, toRegister);
-		attach1.put(toRegister, value1);
-		attach2.put(toRegister, value2);
-		attach3.put(toRegister, value3);
-		attach4.put(toRegister, value4);
-		return toRegister;
+		Registry.register(registry, id, entry);
+		attach1.put(entry, value1);
+		attach2.put(entry, value2);
+		attach3.put(entry, value3);
+		attach4.put(entry, value4);
+		return entry;
 	}
 }
