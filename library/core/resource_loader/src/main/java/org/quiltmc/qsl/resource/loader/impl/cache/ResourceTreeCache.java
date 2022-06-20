ccbd352c171944a30db5aaaeaf0ce89def3eb4fe
LambdAurora <email@lambdaurora.dev>
2022-06-20 17:13:12 +0200

    Add mod resource packs caching for faster reloads.
    Also use the java.io.File API when possible instead of NIO exists.

---
+++ library/core/resource_loader/src/main/java/org/quiltmc/qsl/resource/loader/impl/cache/ResourceTreeCache.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.resource.loader.impl.cache;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.resource.ResourceType;
+
+import org.quiltmc.qsl.resource.loader.impl.ModIoOps;
+
+/**
+ * A variant of {@link ResourceAccess} that also caches I/O queries into a tree.
+ *
+ * @author LambdAurora
+ */
+@ApiStatus.Internal
+public final class ResourceTreeCache extends ResourceAccess {
+	private final CacheTree.Branch tree = CacheTree.newTree();
+	private final Map<ResourceType, Set<String>> namespaces = new EnumMap<>(ResourceType.class);
+
+	public ResourceTreeCache(ModIoOps io) {
+		super(io);
+	}
+
+	@Override
+	public @Nullable Entry getEntry(String pathName) {
+		var node = this.tree.resolveOrCompute(this.io, pathName);
+		if (node != null) {
+			return node.toEntry(this.io);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public Set<String> getNamespaces(ResourceType type) {
+		var namespaces = this.namespaces.get(type);
+
+		if (namespaces != null) {
+			return namespaces;
+		}
+
+		namespaces = super.getNamespaces(type);
+		this.namespaces.put(type, namespaces);
+
+		return namespaces;
+	}
+}
