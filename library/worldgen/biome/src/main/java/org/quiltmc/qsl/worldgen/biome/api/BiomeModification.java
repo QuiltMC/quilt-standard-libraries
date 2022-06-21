61188f3193e8938be84ebaa417bb7a0d5ed6a14e
peaceheis <69596656+peaceheis@users.noreply.github.com>
2022-06-21 18:20:44 -0500

    Pushing <P>
    Co-authored-by: LambdAurora <aurora42lambda@gmail.com>

--- library/worldgen/biome/src/main/java/org/quiltmc/qsl/worldgen/biome/api/BiomeModification.java
+++ library/worldgen/biome/src/main/java/org/quiltmc/qsl/worldgen/biome/api/BiomeModification.java
@@ -51,8 +51,8 @@ public class BiomeModification {
 	/**
 	 * Adds a modifier that is sensitive to the current state of the biome when it is applied.
 	 * Examples for this are modifiers that apply scales to existing values (e.g. half the temperature).
-	 *
-	 * <p>For modifiers that should only be applied if a given condition is met for a Biome, please add these
+	 * <p>
+	 * For modifiers that should only be applied if a given condition is met for a Biome, please add these
 	 * conditions to the selector, and use a context-free modifier instead, as this will greatly help
 	 * with debugging world generation issues.
 	 */
