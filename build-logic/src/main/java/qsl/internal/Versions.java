59cec020b3b7080de397ca24c4c4b7a438417ea0
LambdAurora <email@lambdaurora.dev>
2022-07-09 13:52:37 +0200

    Remove interface injection of QuiltBlockEntity.

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -17,7 +17,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "1.1.0-beta.21";
+	public static final String QSL_VERSION = "1.1.0-beta.22";
 
 	/**
 	 * The target Minecraft version.
