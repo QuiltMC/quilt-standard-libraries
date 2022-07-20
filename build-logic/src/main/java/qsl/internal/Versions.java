3655329bf57abdd621c038745e52cf0bf14c2a1d
LambdAurora <email@lambdaurora.dev>
2022-07-21 00:46:30 +0200

    Fix the sneaky broken TestServerMixin.

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -21,7 +21,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "2.1.0-alpha.2";
+	public static final String QSL_VERSION = "2.1.0-alpha.3";
 
 	/**
 	 * The target Minecraft version.
