6429497277e6bee23a63378a2d8bf84ea02535fa
peaceheis <69596656+peaceheis@users.noreply.github.com>
2022-07-22 16:50:01 -0500

    Singular Import Fix

--- library/management/client_command/src/main/java/org/quiltmc/qsl/command/mixin/client/ClientPlayerEntityMixin.java
+++ library/management/client_command/src/main/java/org/quiltmc/qsl/command/mixin/client/ClientPlayerEntityMixin.java
@@ -19,6 +19,7 @@ package org.quiltmc.qsl.command.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
