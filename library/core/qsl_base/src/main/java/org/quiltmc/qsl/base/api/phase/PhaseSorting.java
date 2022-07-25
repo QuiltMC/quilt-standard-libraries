4cfc41039c429e3763bbea7bb39732d7a6ed3e11
LambdAurora <email@lambdaurora.dev>
2022-07-25 23:53:57 +0200

    Fix up logging.

--- library/core/qsl_base/src/main/java/org/quiltmc/qsl/base/api/phase/PhaseSorting.java
+++ library/core/qsl_base/src/main/java/org/quiltmc/qsl/base/api/phase/PhaseSorting.java
@@ -136,7 +136,7 @@ public final class PhaseSorting {
 		} else if (phase.visitStatus == PhaseData.VisitStatus.VISITING && ENABLE_CYCLE_WARNING) {
 			// Already visiting, so we have found a cycle.
 			QuiltBaseImpl.LOGGER.warn(String.format(
-					"Event phase ordering conflict detected.%nEvent phase %s is ordered both before and after event phase %s.",
+					"Phase ordering conflict detected.%nPhase %s is ordered both before and after phase %s.",
 					phase.id,
 					parent.id
 			));
