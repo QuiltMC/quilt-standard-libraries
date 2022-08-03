96485cb0768182a41cf093d833fef8727667d734
Patbox <39821509+Patbox@users.noreply.github.com>
2022-08-03 18:19:41 +0200

    Apply missing licenses

--- library/core/registry/src/main/java/org/quiltmc/qsl/registry/mixin/client/ItemModelsMixin.java
+++ library/core/registry/src/main/java/org/quiltmc/qsl/registry/mixin/client/ItemModelsMixin.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.quiltmc.qsl.registry.mixin.client;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
