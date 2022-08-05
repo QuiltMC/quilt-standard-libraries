814a2e3d50fda49de585b698ceb0580ccfa65882
LambdAurora <email@lambdaurora.dev>
2022-08-05 09:11:36 +0200

    Fix logging.

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -21,7 +21,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "3.0.0-beta.8";
+	public static final String QSL_VERSION = "3.0.0-beta.9";
 
 	/**
 	 * The target Minecraft version.
