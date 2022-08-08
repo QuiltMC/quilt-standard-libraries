b6eac77ae45340a4111dc84b6ed24d7f577e90de
Platymemo <edwardhazelton+github@gmail.com>
2022-08-07 20:43:10 -0800

    rename POI initializer

--- library/entity/points_of_interest/src/testmod/java/org/quiltmc/qsl/points_of_interest/test/PoiTestMod.java
+++ library/entity/points_of_interest/src/testmod/java/org/quiltmc/qsl/points_of_interest/test/PoiTestMod.java
@@ -25,7 +25,7 @@ import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.points_of_interest.api.PointOfInterestHelper;
 
-public class MainInitializer implements ModInitializer {
+public class PoiTestMod implements ModInitializer {
 	@Override
 	public void onInitialize(ModContainer mod) {
 		PointOfInterestHelper.addBlocks(PointOfInterestTypes.ARMORER, Blocks.ANVIL, Blocks.CHIPPED_ANVIL, Blocks.DAMAGED_ANVIL);
