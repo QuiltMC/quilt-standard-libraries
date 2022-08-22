654d322dd971fca6c604b2ef192c6da6056cc542
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-08-22 18:32:53 +0300

    Remove block ignite stuff

--- library/item/item_extensions/src/main/java/org/quiltmc/qsl/item/extensions/api/event/ItemInteractionEvents.java
+++ library/item/item_extensions/src/main/java/org/quiltmc/qsl/item/extensions/api/event/ItemInteractionEvents.java
@@ -16,32 +16,8 @@
 
 package org.quiltmc.qsl.item.extensions.api.event;
 
-import org.jetbrains.annotations.NotNull;
-
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.util.ActionResult;
-
-import org.quiltmc.qsl.base.api.event.Event;
-import org.quiltmc.qsl.base.api.event.EventAwareListener;
-
 public final class ItemInteractionEvents {
 	private ItemInteractionEvents() {
 		throw new UnsupportedOperationException("BlockInteractionEvents only contains static declarations.");
-	}
-
-	public static final Event<IgniteBlock> IGNITE_BLOCK = Event.create(IgniteBlock.class, callbacks -> context -> {
-		var result = ActionResult.PASS;
-		for (var callback : callbacks) {
-			result = callback.onIgniteBlock(context);
-			if (result != ActionResult.PASS) {
-				return result;
-			}
-		}
-		return result;
-	});
-
-	@FunctionalInterface
-	public interface IgniteBlock extends EventAwareListener {
-		@NotNull ActionResult onIgniteBlock(@NotNull ItemUsageContext context);
 	}
 }
--- library/item/item_extensions/src/testmod/java/org/quiltmc/qsl/item/extensions/test/Initializer.java
+++ library/item/item_extensions/src/testmod/java/org/quiltmc/qsl/item/extensions/test/Initializer.java
@@ -16,36 +16,14 @@
 
 package org.quiltmc.qsl.item.extensions.test;
 
-import org.jetbrains.annotations.NotNull;
-
-import net.minecraft.block.Blocks;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.item.extensions.api.event.ItemInteractionEvents;
 
-public final class Initializer implements ModInitializer,
-		ItemInteractionEvents.IgniteBlock {
+public final class Initializer implements ModInitializer {
 	public static final Initializer INSTANCE = new Initializer();
 
 	private Initializer() {}
 
 	@Override
 	public void onInitialize(ModContainer mod) {}
-
-	@Override
-	public @NotNull ActionResult onIgniteBlock(@NotNull ItemUsageContext context) {
-		if (context.getBlockState().isOf(Blocks.IRON_BLOCK)) {
-			context.playSoundAtBlock(SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS,
-					1.0F, context.getWorldRandom().nextFloat() * 0.4F + 0.8F);
-			context.replaceBlock(Blocks.NETHERITE_BLOCK.getDefaultState());
-			context.damageStack();
-			return context.success();
-		}
-		return ActionResult.PASS;
-	}
 }
