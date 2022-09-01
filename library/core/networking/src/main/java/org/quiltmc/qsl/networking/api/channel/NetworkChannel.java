6fbb2c910ce4cee28b6cf3f88c12a487ade2cb5f
0xJoeMama <0xjoemama@gmail.com>
2022-09-01 12:48:20 +0300

    applyLicenses

--- library/core/networking/src/main/java/org/quiltmc/qsl/networking/api/channel/NetworkChannel.java
+++ library/core/networking/src/main/java/org/quiltmc/qsl/networking/api/channel/NetworkChannel.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.quiltmc.qsl.networking.api.channel;
 
 import java.util.function.Function;
@@ -99,7 +115,7 @@ public interface NetworkChannel<T> {
 			Supplier<Function<T, C2SNetworkChannel.Handler>> c2sHandlerProvider,
 			Supplier<Function<T, S2CNetworkChannel.Handler>> s2cHandlerProvider
 	) {
-		return new TwoWayNetworkChannel<T>(id, codec, c2sHandlerProvider, s2cHandlerProvider);
+		return new TwoWayNetworkChannel<>(id, codec, c2sHandlerProvider, s2cHandlerProvider);
 	}
 
 	/**
