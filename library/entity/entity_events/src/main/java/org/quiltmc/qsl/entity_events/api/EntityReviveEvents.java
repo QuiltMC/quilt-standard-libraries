aa226fbb92347494de0eb07968df2118e5c02a3d
William Bradford Larcombe <william@williambl.com>
2022-10-05 11:28:29 +0100

    Entity Events module (#42)
    * Add entity events module
    
    * Add TryReviveCallback
    based on my work in FabricMC/fabric#1394
    
    * Add EntityTickCallback
    
    * Add AfterKilledOtherEntityCallback
    Based on Fabric's ServerCombatEvents.AFTER_KILLED_OTHER_ENTITY
    
    * Fix client entity tick event
    
    * Rename AfterKilledOtherEntityCallback -> EntityKilledCallback
    
    * Add entity events test mod
    
    * Add entity load/unload events.
    
    * Add ServerEntityWorldChangeEvents
    Direct port of @i509VCB's ones for Fabric
    
    * Apply javadoc changes
    
    * Split TryReviveCallback into two callback interfaces
    
    * Fix entity library requiring wrong loader
    
    * Update entity events to 1.18
    
    * Remove entity tick event, to be split into its own PR
    
    * Split entity load events sidedly
    
    * Rename ServerEntityWorldChangeEvents -> EntityWorldChangeEvents
    
    * Add note about getRecentDamageSource() to EntityKilledCallback
    
    * Fix license headers for entity events
    
    * Split off client entity load events into new class
    
    * Rename methods for entity load events on client
    
    * Add ServerPlayerEntityCopyCallback
    
    * Add license to ServerPlayerEntityCopyCallback
    
    * ArrayEvent -> Event
    
    * Make a few renames
    
    * Rename entity-events -> entity_events
    
    * Use new way of declaring deps in entity_events module
    
    * Change package of entity events module
    
    * Fix entity events metadata
    
    * Switch entity events test mod to new entrypoints
    
    * Use var in enhanced-for loops in entity events
    
    * Make entity events aware listeners
    
    * Fix licenses.
    
    * Apply @TheGlitch76 javadoc suggestion #1
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * Apply @TheGlitch76 javadoc suggestion #2
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * Apply @TheGlitch76 javadoc suggestion #3
    
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    
    * Fix entity events qmj
    
    * Replace EntityKilledCallback with LivingEntityDeathCallback
    
    * Implement @TropheusJ's suggestions
    
    * Fix TeleportCommand mixin localcapture
    
    * Remove version from entity events build.gradle
    
    * Fix entity events for 1.19
    
    * Apply licenses to entity events
    
    * Fix checkstyle in entity events
    
    * Replace fabric.mod.json with quilt.mod.json in entity events testmod
    
    * Fix javadoc in entity events
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/ServerPlayerEntityCopyCallback.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/LivingEntityDeathCallback.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Rename some entity world change events
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/EntityReviveEvents.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/EntityWorldChangeEvents.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/EntityWorldChangeEvents.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/EntityReviveEvents.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/EntityWorldChangeEvents.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Update library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/EntityReviveEvents.java
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    Co-authored-by: LambdAurora <email@lambdaurora.dev>
    Co-authored-by: Glitch <glitch.g3431@gmail.com>
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>

---
+++ library/entity/entity_events/src/main/java/org/quiltmc/qsl/entity_events/api/EntityReviveEvents.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2021-2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.entity_events.api;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+
+import org.quiltmc.qsl.base.api.event.Event;
+import org.quiltmc.qsl.base.api.event.EventAwareListener;
+
+/**
+ * A pair of callbacks which are invoked when a LivingEntity takes fatal damage.
+ * <p>
+ * Mods can return true from a callback to keep the entity alive, like totems of undying do. This will also prevent
+ * {@link LivingEntityDeathCallback} listeners from being called.
+ * <p>
+ * Vanilla checks for entity health {@code <= 0} each tick (with
+ * {@link net.minecraft.entity.LivingEntity#isDead()}), and kills the entity if this is true - so the entity will
+ * still die next tick if this event is cancelled. It's assumed that the listener will do something to prevent this,
+ * for example:
+ * <ul>
+ *     <li>a totem-of-undying mod giving the player a little health + regeneration effect</li>
+ *     <li>a minigame mod teleporting the player into a 'respawn room' and setting their health to 20.0</li>
+ *     <li>a mod that changes death mechanics switching the player over to the mod's play-mode, where death doesn't
+ *     apply</li>
+ * </ul>
+ * <p>
+ * These events are short-circuiting - if the entity has already been revived, your callback will not fire.
+ */
+public final class EntityReviveEvents {
+	/**
+	 * Called before totems try to revive the entity.
+	 */
+	public static Event<TryReviveBeforeTotem> BEFORE_TOTEM = Event.create(TryReviveBeforeTotem.class, callbacks -> (entity, damageSource) -> {
+		for (var callback : callbacks) {
+			if (callback.tryReviveBeforeTotem(entity, damageSource)) {
+				return true;
+			}
+		}
+
+		return false;
+	});
+
+	/**
+	 * Called after there has been an attempt to revive with totems, but it has not been successful.
+	 */
+	public static Event<TryReviveAfterTotem> AFTER_TOTEM = Event.create(TryReviveAfterTotem.class, callbacks -> (entity, damageSource) -> {
+		for (var callback : callbacks) {
+			if (callback.tryReviveAfterTotem(entity, damageSource)) {
+				return true;
+			}
+		}
+
+		return false;
+	});
+
+	@FunctionalInterface
+	public interface TryReviveBeforeTotem extends EventAwareListener {
+		/**
+		 * Returns whether an entity which has fatal damage should be revived.
+		 *
+		 * @param entity       the entity
+		 * @param damageSource the fatal damage source
+		 * @return true if the entity should be revived, false otherwise.
+		 */
+		boolean tryReviveBeforeTotem(LivingEntity entity, DamageSource damageSource);
+	}
+
+	@FunctionalInterface
+	public interface TryReviveAfterTotem extends EventAwareListener {
+		/**
+		 * Returns whether an entity which has fatal damage and which has not been saved by a totem should be revived.
+		 *
+		 * @param entity       the entity
+		 * @param damageSource the fatal damage source
+		 * @return true if the entity should be revived, false otherwise.
+		 */
+		boolean tryReviveAfterTotem(LivingEntity entity, DamageSource damageSource);
+	}
+
+	private EntityReviveEvents() {}
+}
