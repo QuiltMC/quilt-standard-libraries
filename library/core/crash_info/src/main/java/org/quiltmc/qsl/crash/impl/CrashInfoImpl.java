10a4526b13de050794dad11d582cca0c3cdab90a
AlexIIL <alexj9@me.com>
2022-10-12 21:36:40 +0100

    Turns out we need a newline otherwise it doesn't work.

--- library/core/crash_info/src/main/java/org/quiltmc/qsl/crash/impl/CrashInfoImpl.java
+++ library/core/crash_info/src/main/java/org/quiltmc/qsl/crash/impl/CrashInfoImpl.java
@@ -29,6 +29,6 @@ import org.quiltmc.qsl.crash.api.CrashReportEvents;
 public final class CrashInfoImpl implements CrashReportEvents.SystemDetails {
 	@Override
 	public void addDetails(SystemDetails details) {
-		details.addSection("Quilt Mods", QuiltLoader::createModTable);
+		details.addSection("Quilt Mods", () -> "\n" + QuiltLoader.createModTable());
 	}
 }
