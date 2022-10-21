8e5deff8b9794cf9e4dcc6cb5f49e5f805560ffd
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2022-10-21 21:35:11 +0200

    Change `enchantment_booster` rea namespace

--- library/block/block_content_registry/src/main/java/org/quiltmc/qsl/block/content/registry/api/BlockContentRegistries.java
+++ library/block/block_content_registry/src/main/java/org/quiltmc/qsl/block/content/registry/api/BlockContentRegistries.java
@@ -48,6 +48,7 @@ public class BlockContentRegistries {
 	 * The namespace for the content registries.
 	 */
 	public static final String NAMESPACE = "quilt_block_content_registry";
+	public static final String QUILT_NAMESPACE = "quilt";
 
 	/**
 	 * A {@link RegistryEntryAttachment} for when blocks are right clicked by a shovel.
@@ -134,11 +135,11 @@ public class BlockContentRegistries {
 	/**
 	 * A {@link RegistryEntryAttachment} for enchantment power levels in bookshelf equivalents.
 	 * <p>
-	 * Values can be set via code and through a data-pack with the file {@code data/quilt_block_content_registry/attachments/minecraft/block/enchantment_booster.json}
+	 * Values can be set via code and through a data-pack with the file {@code data/quilt/attachments/minecraft/block/enchantment_booster.json}
 	 */
 	public static final RegistryEntryAttachment<Block, Float> ENCHANTMENT_BOOSTER = RegistryEntryAttachment
 			.builder(Registry.BLOCK,
-					new Identifier(NAMESPACE, "enchantment_booster"),
+					new Identifier(QUILT_NAMESPACE, "enchantment_booster"),
 					Float.class,
 					Codec.floatRange(0f, Float.MAX_VALUE))
 			.build();
