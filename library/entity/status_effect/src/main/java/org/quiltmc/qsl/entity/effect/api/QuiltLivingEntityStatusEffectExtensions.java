66c685d7b1414daa85a036ce3ecaa10a5f514ae4
ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
2022-11-13 17:25:54 +0200

    Status Effects API (#187)
    * Status Effects API
    
    * status_effects -> status_effect
    
    * Extend StatusEffectRemovalReason
    
    * Fix autocomplete hiccup
    
    Co-authored-by: Whangd00dle <67663207+Platymemo@users.noreply.github.com>
    
    * Rename QuiltLivingEntityExtensions to QuiltLivingEntityStatusEffectExtensions
    
    * Add QuiltStatusEffectExtensions.onRemoved
    
    * Remove duplicate assignments
    
    * Prevent removal reasons from sticking
    
    * Convert StatusEffectRemovalReason to a class
    
    Rename *_SPECIFIC reasons to *_ONE
    
    * Javadoc it up
    
    * StatusEffectRemovalReason: replace removesEffectType with removesEffect
    
    * Testmod!
    
    * licenses
    
    * LivingEntityMixin: remove unnecessary throw in constructor
    
    * StatusEffectRemovalReason: change javadoc for UNKNOWN
    
    * Okay, NOW this gets called after the vanilla method
    
    * Add "should remove status effect" event
    
    * the license is real
    
    * damn you copy/paste
    
    Co-authored-by: Whangd00dle <67663207+Platymemo@users.noreply.github.com>
    
    * LivingEntityMixin: move shadowed methods to the top
    
    * Replace vanilla StatusEffect.onRemove instead of adding another callback after it
    
    (default impl is to call the vanilla method)
    
    * Move "unknown" removal reason to impl class
    
    Deduplicate identifier namespaces
    
    * I should really set up a git hook for applyLicenses
    
    * do you see my point
    
    * Javadoc fixups
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Add removal reasons for effects expiring and reapplying
    
    * QuiltStatusEffectExtensions.onRemoved: receive the StatusEffectInstance
    
    * Add ON_APPLIED and ON_REMOVED events
    
    * Fix ON_APPLIED's upgradeReapplying parameter always being true
    
    Co-authored-by: Whangd00dle <67663207+Platymemo@users.noreply.github.com>
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>

---
+++ library/entity/status_effect/src/main/java/org/quiltmc/qsl/entity/effect/api/QuiltLivingEntityStatusEffectExtensions.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.entity.effect.api;
+
+import org.jetbrains.annotations.NotNull;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+
+import org.quiltmc.qsl.base.api.util.InjectedInterface;
+
+/**
+ * Provides status effect-related extensions to the {@link LivingEntity} class.
+ */
+@InjectedInterface(LivingEntity.class)
+public interface QuiltLivingEntityStatusEffectExtensions {
+	/**
+	 * Removes a status effect of a specific type.
+	 *
+	 * @param type   the type of status effect to remove
+	 * @param reason the reason to remove the status effect
+	 * @return {@code true} if the status effect was successfully removed, or {@code false} otherwise.
+	 */
+	boolean removeStatusEffect(@NotNull StatusEffect type, @NotNull StatusEffectRemovalReason reason);
+
+	/**
+	 * Removes all status effects.
+	 *
+	 * @param reason the reason to remove the status effects
+	 * @return the number of status effects that were successfully removed.
+	 */
+	int clearStatusEffects(@NotNull StatusEffectRemovalReason reason);
+
+	/**
+	 * Should be called when a status effect is removed.
+	 *
+	 * @param effect the effect that was removed
+	 * @param reason the reason the effect was removed
+	 */
+	void onStatusEffectRemoved(@NotNull StatusEffectInstance effect, @NotNull StatusEffectRemovalReason reason);
+}
