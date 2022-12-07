b2a76cac70df9c586db49c585aa5a5c17f1bef9a
LambdAurora <email@lambdaurora.dev>
2022-12-07 18:20:32 +0100

    Bump version.

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -23,7 +23,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "3.0.0-beta.23";
+	public static final String QSL_VERSION = "3.0.0-beta.24";
 
 	/**
 	 * The target Minecraft version.
