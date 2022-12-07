0bf91bdbbfc67f15d05cb75b1e3d83ebc9e42513
LambdAurora <email@lambdaurora.dev>
2022-12-07 21:30:32 +0100

    Fix error message in actual test too.

--- library/core/game_test/src/testmod/java/org/quiltmc/qsl/game_test/test/ExampleTestSuite.java
+++ library/core/game_test/src/testmod/java/org/quiltmc/qsl/game_test/test/ExampleTestSuite.java
@@ -32,7 +32,11 @@ public class ExampleTestSuite implements QuiltGameTest {
 		context.setBlockState(0, 2, 0, Blocks.DIAMOND_BLOCK);
 
 		context.addInstantFinalTask(() ->
-				context.checkBlock(new BlockPos(0, 2, 0), (block) -> block == Blocks.DIAMOND_BLOCK, "Expect block to be diamond")
+				context.checkBlock(
+						new BlockPos(0, 2, 0),
+						(block) -> block == Blocks.DIAMOND_BLOCK,
+						"Expected block to be diamond"
+				)
 		);
 	}
 }
