0dd6870d0b87fd215fde6a42bcd5b30869cbc249
williambl <william@williambl.com>
2023-01-05 23:24:27 +0000

    chore: apply licenses + formatting to armor knockback res code

--- library/item/item_extensions/src/testmod/java/org/quiltmc/qsl/item/extensions/test/ArmorKnockbackTest.java
+++ library/item/item_extensions/src/testmod/java/org/quiltmc/qsl/item/extensions/test/ArmorKnockbackTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.quiltmc.qsl.item.extension.test;
 
 import static org.quiltmc.qsl.item.extension.test.BowsTest.MOD_ID;
@@ -7,7 +23,6 @@ import java.util.List;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
