bc42186b4ccc89ef108454bd22a1777d9b368ba6
Silver <52360088+SilverAndro@users.noreply.github.com>
2023-01-22 17:45:47 -0500

    Add a chat API (#250)
    * First bit of work, client side is functional!
    
    All inbound events are still on the networking thread, maybe should inject *after* main thread is forced to prevent bugs?
    
    * Server side + mappings bump
    
    * Cleanup mixin injections, more test mod
    
    * Cleanup and clarity, begin work on last message type
    
    * Client side inbound profile independent messages
    
    * Move some stuff around+renames
    
    * HUGE amount of work rewriting this!
    
    * TIL I learned java doesnt have import alias
    
    * and with the power of flex tape, I ~~sawed~~ sealed this type system!
    
    * Cleanup, get game launching, more hooks
    
    * More before/after IO hooks
    
    * More cleanup and work, almost at done!
    
    * Apparently that wasnt needed!
    
    * Add some more test:tm:
    
    * Didnt forget these shush
    
    * Flatten initializer in testmod qmj
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Cleanup + resolve
    
    * Un-seal message wrappers, prevent instantiating QuiltChatEvents and InternalQuiltChatApiUtil
    
    * Rewrite everything!
    
    Everything is immutable now with `withX` methods for modification. MODIFY now returns a new instance collected from the listeners, or the original if nothing is returned
    
    * Fix events, toString issues, some more docs
    
    * Fix bug with Void chat events add raw chat message handling
    
    * Move events to API, docs, licensing
    
    * Let testmod conditions fall through
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * More testmod conditional fallthrough
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Fix testmod from suggestions
    
    * Some doc fixes, `var` and space between cast
    
    * Weird docs but alright
    
    * Return cleanup
    
    * Remove testmod gunk
    
    * More return cleanup
    
    * StringBuilder -> String concat
    
    * minor javadoc fix
    
    * Missed a `var`
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Hopefully last bit of cleanup
    
    * _ moment
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    * Attempt to address most concerns
    
    * Ternaries, whoops
    
    * Who needs legal notices anyways smh smh
    
    * Wrong function type, whoops
    
    Hopefully last commit? lmao
    
    * Fix minor mistakes, minor doc changes
    
    * More docs + some tweaks
    
    * These docs are cursed but I think this is how you do it??
    
    Also an NPE fix + clearer naming in a few cases
    
    * Add some more basic docs
    
    * Handler -> Callback
    
    * wow!
    
    * Add CANCELED event
    
    * Final javadoc tweak
    
    * Took the L there ngl
    
    * Javadoc fixes, method contracts
    
    * `this.sendKeypress(Key.MOD_CTRL | Key.V)`
    
    aka add `this.` to everything
    
    * forgor the impl
    
    * I should really just, double check my stuff before pushing
    
    * Bring all mixins in line with naming scheme
    
    * `this.`, naming clarity, split event callbacks
    
    * More Ls for me
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
    
    Co-authored-by: Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>

---
+++ library/management/chat/src/main/java/org/quiltmc/qsl/chat/api/QuiltMessageType.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2023 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.chat.api;
+
+/**
+ * Enums that are used to identify the type, side, and direction of various messages.
+ */
+public enum QuiltMessageType {
+	// Actual types
+
+	/**
+	 * Identifies a chat message. Usually from a player.
+	 */
+	CHAT(QuiltMetaMessageType.MESSAGE_TYPE),
+
+	/**
+	 * Identifies a system message. Used for command results, actionbar messages, and more.
+	 */
+	SYSTEM(QuiltMetaMessageType.MESSAGE_TYPE),
+
+	/**
+	 * Identifies a profile independent message. Usually from /msg and similar commands.
+	 */
+	PROFILE_INDEPENDENT(QuiltMetaMessageType.MESSAGE_TYPE),
+
+	// Sidedness
+
+	/**
+	 * Identifies a message that is on the server currently.
+	 */
+	SERVER(QuiltMetaMessageType.SIDE),
+
+	/**
+	 * Identifies a message that is on the client currently.
+	 */
+	CLIENT(QuiltMetaMessageType.SIDE),
+
+	// Directionality
+
+	/**
+	 * Identifies a message that is arriving on the side it is on currently.
+	 */
+	INBOUND(QuiltMetaMessageType.DIRECTION),
+
+	/**
+	 * Identifies a message that is leaving the side it is on currently.
+	 */
+	OUTBOUND(QuiltMetaMessageType.DIRECTION);
+
+	public final QuiltMetaMessageType metaType;
+
+	QuiltMessageType(QuiltMetaMessageType metaType) {
+		this.metaType = metaType;
+	}
+
+	/**
+	 * The various meta-types of the {@link QuiltMessageType} enums. Used for categorizing the different types.
+	 */
+	public enum QuiltMetaMessageType {
+		/**
+		 * Identifies the message type of a message.
+		 */
+		MESSAGE_TYPE,
+
+		/**
+		 * Identifies the side a message is currently on.
+		 */
+		SIDE,
+
+		/**
+		 * Identifies if the message is inbound or outbound from the current side.
+		 */
+		DIRECTION
+	}
+}
---
+++ library/management/chat/src/main/java/org/quiltmc/qsl/chat/impl/ChatEventBooleanImpl.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2023 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.qsl.chat.impl;
+
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+import org.quiltmc.qsl.base.api.event.Event;
+import org.quiltmc.qsl.chat.api.ChatEvent;
+import org.quiltmc.qsl.chat.api.QuiltMessageType;
+import org.quiltmc.qsl.chat.api.types.AbstractChatMessage;
+
+import java.util.EnumSet;
+import java.util.function.BiFunction;
+
+/**
+ * An implementation of {@link ChatEvent} that always returns a {@link Boolean} and has special short circuiting logic for if a callback returns true.
+ * This implementation is intended be used for any cancellation events.
+ */
+public class ChatEventBooleanImpl<C> implements ChatEvent<C, Boolean> {
+	private final BiFunction<C, EnumSet<QuiltMessageType>, TypedChatApiHook<Boolean>> converter;
+	private final Event<TypedChatApiHook<Boolean>> backingEvent = Event.create(TypedChatApiHook.class, hooks -> new TypedChatApiHook<>() {
+		@Override
+		public EnumSet<QuiltMessageType> getMessageTypes() {
+			return EnumSet.allOf(QuiltMessageType.class);
+		}
+
+		@Override
+		public Boolean handleMessage(@NotNull AbstractChatMessage<?> message) {
+			for (var hook : hooks) {
+				if (shouldPassOnMessageToHook(message.getTypes(), hook.getMessageTypes())) {
+					if (hook.handleMessage(message)) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	});
+
+	public ChatEventBooleanImpl(BiFunction<C, EnumSet<QuiltMessageType>, TypedChatApiHook<Boolean>> converter) {
+		this.converter = converter;
+	}
+
+	private boolean shouldPassOnMessageToHook(EnumSet<QuiltMessageType> messageTypes, EnumSet<QuiltMessageType> hookTypes) {
+		// For every message type
+		for (var messageType : messageTypes) {
+			// If the hook isn't looking for it
+			if (!hookTypes.contains(messageType)) {
+				// If it doesn't match the complex rule
+				if (!this.matchesMetaTypeRule(messageType.metaType, hookTypes)) {
+					// Not a match
+					return false;
+				}
+			}
+		}
+
+		// All message types are wanted, pass it on
+		return true;
+	}
+
+	private boolean matchesMetaTypeRule(QuiltMessageType.QuiltMetaMessageType metaType, EnumSet<QuiltMessageType> hookTypes) {
+		// For every type the hook is looking for
+		for (var hookType : hookTypes) {
+			// Check if they have the same meta type
+			if (hookType.metaType == metaType) {
+				// If so, don't pass it on
+				// We eliminated equal type previously, so this is only same-meta-different-type
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public Boolean invoke(@NotNull AbstractChatMessage<?> message) {
+		return this.backingEvent.invoker().handleMessage(message);
+	}
+
+	@Override
+	public Boolean invokeOrElse(@NotNull AbstractChatMessage<?> message, @NotNull Boolean ifNull) {
+		Boolean result = this.backingEvent.invoker().handleMessage(message);
+		return result != null ? result : ifNull;
+	}
+
+	@Override
+	public void register(@NotNull EnumSet<QuiltMessageType> types, @NotNull C callback) {
+		this.backingEvent.register(this.converter.apply(callback, types));
+	}
+
+	@Override
+	public void register(@NotNull Identifier phaseIdentifier, @NotNull EnumSet<QuiltMessageType> types, @NotNull C callback) {
+		this.backingEvent.register(this.converter.apply(callback, types));
+	}
+
+	@Override
+	public void addPhaseOrdering(@NotNull Identifier firstPhase, @NotNull Identifier secondPhase) {
+		this.backingEvent.addPhaseOrdering(firstPhase, secondPhase);
+	}
+}
