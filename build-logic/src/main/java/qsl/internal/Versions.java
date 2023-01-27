4c0639c0e71e0eef2b0c29e8b0c177cbd71b6be5
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2023-01-26 21:43:40 -0300

    Fix lack of injected interface glue on QuiltArmorMaterialExtensions

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -23,7 +23,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "3.0.0-beta.27";
+	public static final String QSL_VERSION = "3.0.0-beta.28";
 
 	/**
 	 * The target Minecraft version.
