300b36ce46fa2476b281cb9856c99936b3bfb278
Luke Bemish <lukebemish@lukebemish.dev>
2023-02-28 16:40:29 -0600

    Fix duplicate codec identifier

--- library/worldgen/biome/src/main/java/org/quiltmc/qsl/worldgen/biome/api/codec/RemoveSpawnersModifier.java
+++ library/worldgen/biome/src/main/java/org/quiltmc/qsl/worldgen/biome/api/codec/RemoveSpawnersModifier.java
@@ -36,7 +36,7 @@ import org.quiltmc.qsl.worldgen.biome.api.BiomeSelectionContext;
 public record RemoveSpawnersModifier(CodecAwarePredicate<BiomeSelectionContext> selector,
 								  Set<Identifier> entityTypes,
 								  Set<SpawnGroup> groups) implements BiomeModifier {
-	public static final Identifier IDENTIFIER = new Identifier("quilt", "add_spawners");
+	public static final Identifier IDENTIFIER = new Identifier("quilt", "remove_spawners");
 	public static final Codec<RemoveSpawnersModifier> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 			BiomeModifier.BIOME_SELECTOR_CODEC.fieldOf("selector").forGetter(RemoveSpawnersModifier::selector),
 			CodecHelpers.listOrValue(Identifier.CODEC).xmap(Set::copyOf, List::copyOf).fieldOf("entity_types").forGetter(RemoveSpawnersModifier::entityTypes),
