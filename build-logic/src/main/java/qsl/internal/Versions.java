a87ef71ae47e10b9e19805c0e2f50a6e11a437f0
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2023-03-04 15:37:15 -0300

    Update to Quilt Loader 0.18.3

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -43,7 +43,7 @@ public final class Versions {
 	/**
 	 * The version of Quilt Loader to use.
 	 */
-	public static final String LOADER_VERSION = "0.17.10";
+	public static final String LOADER_VERSION = "0.18.3";
 
 	/**
 	 * The target Java version.
