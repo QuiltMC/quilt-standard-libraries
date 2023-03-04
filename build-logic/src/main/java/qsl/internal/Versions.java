bf1b26ef484d9eae479c3864de1bd212f7b98c32
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2023-03-04 16:30:04 -0300

    Revert "Update to Quilt Loader 0.18.3"
    This reverts commit a87ef71ae47e10b9e19805c0e2f50a6e11a437f0.

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -43,7 +43,7 @@ public final class Versions {
 	/**
 	 * The version of Quilt Loader to use.
 	 */
-	public static final String LOADER_VERSION = "0.18.3";
+	public static final String LOADER_VERSION = "0.17.10";
 
 	/**
 	 * The target Java version.
