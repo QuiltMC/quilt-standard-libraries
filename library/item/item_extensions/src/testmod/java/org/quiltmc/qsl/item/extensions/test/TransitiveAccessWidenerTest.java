d3fca7f9c99775310f58af2d88ca5ebfc42c2113
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2023-05-15 20:11:22 -0300

    Make music disc consistent with music discs

--- library/item/item_extensions/src/testmod/java/org/quiltmc/qsl/item/extensions/test/TransitiveAccessWidenerTest.java
+++ library/item/item_extensions/src/testmod/java/org/quiltmc/qsl/item/extensions/test/TransitiveAccessWidenerTest.java
@@ -22,13 +22,14 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 public class TransitiveAccessWidenerTest implements ModInitializer {
 	public static final Item MODDED_MUSIC_DISC = new MusicDiscItem(
-		5, SoundEvents.BLOCK_COMPOSTER_READY, new Item.Settings().maxCount(1), 4
+		5, SoundEvents.BLOCK_COMPOSTER_READY, new Item.Settings().maxCount(1).rarity(Rarity.RARE), 4
 	);
 
 	@Override
