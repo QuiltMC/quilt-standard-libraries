01ff11cd5ead5b512aa20be505fad82dedb87729
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2023-05-30 19:26:50 -0300

    Bump version

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -23,7 +23,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "5.0.0-beta.9";
+	public static final String QSL_VERSION = "5.0.0-beta.10";
 
 	/**
 	 * The target Minecraft version.
