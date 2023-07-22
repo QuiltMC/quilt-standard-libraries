edebf129baaadbd081b30d05585852dc816e9910
Jamalam <james@jamalam.tech>
2023-07-22 10:56:40 +0000

    Fix the %s issues (#328)
    closes: #325

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -38,7 +38,7 @@ public final class Versions {
 	/**
 	 * The target Quilt Mappings build.
 	 */
-	public static final int MAPPINGS_BUILD = 10;
+	public static final int MAPPINGS_BUILD = 20;
 
 	/**
 	 * The version of Quilt Loader to use.
