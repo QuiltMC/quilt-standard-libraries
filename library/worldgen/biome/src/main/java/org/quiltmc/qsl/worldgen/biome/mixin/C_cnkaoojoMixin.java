48321274cff475e54a6f003f8d87373afdfb75e6
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2023-07-29 01:06:53 -0300

    Fix missing license

--- library/worldgen/biome/src/main/java/org/quiltmc/qsl/worldgen/biome/mixin/C_cnkaoojoMixin.java
+++ library/worldgen/biome/src/main/java/org/quiltmc/qsl/worldgen/biome/mixin/C_cnkaoojoMixin.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2016, 2017, 2018, 2019 FabricMC
- * Copyright 2023 QuiltMC
+ * Copyright 2023 The Quilt Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
