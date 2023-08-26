41a1722fc285a023bc0e5ebe9c396d51849db2ad
Nemuri <45180890+Nemuri404@users.noreply.github.com>
2023-08-26 10:51:45 -0400

    Create AbstractClientPlayerEntityMixin.java

---
+++ library/item/item_extensions/src/main/java/org/quiltmc/qsl/item/extensions/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -0,0 +1,27 @@
+package org.quiltmc.qsl.item.extensions.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+import org.quiltmc.qsl.item.extensions.api.bow.BowExtensions;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public abstract class AbstractClientPlayerEntityMixin {
+	// Make sure that the fov is changed for custom items
+	@Redirect(
+		method = "getSpeed",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z")
+	)
+	private static boolean renderFov(ItemStack instance, Item item) {
+		if (item == Items.BOW) {
+			return instance.getItem() instanceof BowExtensions; // Return bow for fov
+		}
+		return instance.isOf(item); // Default behavior
+	}
+}
