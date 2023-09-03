e0dbbbc6aa1f81d6a0821cd38a9458c112fc2ee3
Nemuri <45180890+Nemuri404@users.noreply.github.com>
2023-09-03 11:55:58 -0400

    Update AbstractClientPlayerEntityMixin.java
    add max draw duration thingy

--- library/item/item_extensions/src/main/java/org/quiltmc/qsl/item/extensions/mixin/bow/client/AbstractClientPlayerEntityMixin.java
+++ library/item/item_extensions/src/main/java/org/quiltmc/qsl/item/extensions/mixin/bow/client/AbstractClientPlayerEntityMixin.java
@@ -2,17 +2,26 @@ package org.quiltmc.qsl.item.extensions.mixin.bow.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import org.quiltmc.qsl.item.extensions.api.bow.BowExtensions;
 
 @Mixin(AbstractClientPlayerEntity.class)
-public abstract class AbstractClientPlayerEntityMixin {
+public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
+	public AbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+		super(world, pos, yaw, gameProfile);
+	}
+	
 	// Make sure that the fov is changed for custom items
 	@Redirect(
 		  method = "getSpeed",
@@ -23,5 +32,14 @@ public abstract class AbstractClientPlayerEntityMixin {
 			return instance.getItem() instanceof BowExtensions; // Return bow for fov
 		}
 		return instance.isOf(item); // Default behavior
+	}
+
+	// Modify the draw duration value
+	@ModifyConstant(method = "getSpeed", constant = @Constant(floatValue = 20.0F))
+	private float modifyDrawDuration(float constant) {
+		if ((float) ((BowExtensions) this.getActiveItem().getItem()).getMaxDrawDuration() != constant) {
+			return (float) ((BowExtensions) this.getActiveItem().getItem()).getMaxDrawDuration(); // Return custom bow's max draw duration
+		}
+		return constant; // Default behavior
 	}
 }
