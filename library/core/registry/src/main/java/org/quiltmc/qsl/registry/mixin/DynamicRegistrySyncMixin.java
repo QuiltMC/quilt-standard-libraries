696a299da5b19da843790714d10194a5763f2653
Cocona <pexium128@gmail.com>
2023-09-28 13:34:17 -0400

    Ran the licenser. Dear gods that's a lotta copyright year changes wowie

--- library/core/registry/src/main/java/org/quiltmc/qsl/registry/mixin/DynamicRegistrySyncMixin.java
+++ library/core/registry/src/main/java/org/quiltmc/qsl/registry/mixin/DynamicRegistrySyncMixin.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2016, 2017, 2018, 2019 FabricMC
- * Copyright 2023 The Quilt Project
+ * Copyright 2023 QuiltMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
