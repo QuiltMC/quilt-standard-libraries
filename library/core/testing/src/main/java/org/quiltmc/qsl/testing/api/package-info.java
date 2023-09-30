ecaf12696ceaf3b064c43e31eff150d3749e6c83
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2023-09-30 20:57:24 -0300

    Backport license changes

--- library/core/testing/src/main/java/org/quiltmc/qsl/testing/api/package-info.java
+++ library/core/testing/src/main/java/org/quiltmc/qsl/testing/api/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 QuiltMC
+ * Copyright 2023 The Quilt Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
