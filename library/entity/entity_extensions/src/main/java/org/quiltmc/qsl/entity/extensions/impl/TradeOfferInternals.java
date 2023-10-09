c44d01ae6e7393a1ce4845cdc056475559c7e19e
glitch <glitch.g3431@gmail.com>
2023-10-09 18:57:20 -0500

    Update villager constructors to support rebalanced types (#350)
    * Update villager constructors to support rebalanced types
    
    * Update library/entity/entity_extensions/src/main/java/org/quiltmc/qsl/entity/extensions/impl/TradeOfferInternals.java
    
    ---------
    
    Co-authored-by: Eli Orona <eliorona@live.com>

--- library/entity/entity_extensions/src/main/java/org/quiltmc/qsl/entity/extensions/impl/TradeOfferInternals.java
+++ library/entity/entity_extensions/src/main/java/org/quiltmc/qsl/entity/extensions/impl/TradeOfferInternals.java
@@ -18,26 +18,58 @@
 package org.quiltmc.qsl.entity.extensions.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerProfession;
+import org.quiltmc.qsl.entity.extensions.api.TradeOfferHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@ApiStatus.Internal
 public final class TradeOfferInternals {
-	// synchronized guards against concurrent modifications - Vanilla does not mutate the underlying arrays,
+	private static final Logger LOGGER = LoggerFactory.getLogger("quilt-entity-extensions");
+
+	private TradeOfferInternals() {
+	}
+
+	/**
+	 * Make the rebalanced profession map modifiable, then copy all vanilla
+	 * professions' trades to prevent modifications from propagating to the rebalanced one.
+	 */
+	private static void initVillagerTrades() {
+		if (!(TradeOffers.field_45128 instanceof HashMap)) {
+			Map<VillagerProfession, Int2ObjectMap<TradeOffers.Factory[]>> map = new HashMap<>(TradeOffers.field_45128);
+
+			for (Map.Entry<VillagerProfession, Int2ObjectMap<TradeOffers.Factory[]>> trade : TradeOffers.PROFESSION_TO_LEVELED_TRADE.entrySet()) {
+				if (!map.containsKey(trade.getKey())) map.put(trade.getKey(), trade.getValue());
+			}
+
+			TradeOffers.field_45128 = map;
+		}
+	}
+
+	// synchronized guards against concurrent modifications - Vanilla does not mutate the underlying arrays (as of 1.16),
 	// so reads will be fine without locking.
-	public static synchronized void registerVillagerOffers(@NotNull VillagerProfession profession, int level, Consumer<List<TradeOffers.Factory>> factory) {
+	public static synchronized void registerVillagerOffers(VillagerProfession profession, int level, TradeOfferHelper.VillagerOffersAdder factory) {
 		Objects.requireNonNull(profession, "VillagerProfession may not be null.");
-		registerOffers(TradeOffers.PROFESSION_TO_LEVELED_TRADE.computeIfAbsent(profession, key -> new Int2ObjectOpenHashMap<>()), level, factory);
+		initVillagerTrades();
+		registerOffers(TradeOffers.PROFESSION_TO_LEVELED_TRADE.computeIfAbsent(profession, key -> new Int2ObjectOpenHashMap<>()), level, trades -> factory.onRegister(trades, false));
+		registerOffers(TradeOffers.field_45128.computeIfAbsent(profession, key -> new Int2ObjectOpenHashMap<>()), level, trades -> factory.onRegister(trades, true));
 	}
 
 	public static synchronized void registerWanderingTraderOffers(int level, Consumer<List<TradeOffers.Factory>> factory) {
@@ -54,5 +86,68 @@ public final class TradeOfferInternals {
 
 		final TradeOffers.Factory[] allEntries = ArrayUtils.addAll(originalEntries, addedEntries);
 		leveledTradeMap.put(level, allEntries);
+	}
+
+	public static void printRefreshOffersWarning() {
+		Throwable loggingThrowable = new Throwable();
+		LOGGER.warn("TradeOfferHelper#refreshOffers does not do anything, yet it was called! Stack trace:", loggingThrowable);
+	}
+
+	public static class WanderingTraderOffersBuilderImpl implements TradeOfferHelper.WanderingTraderOffersBuilder {
+		private static final Object2IntMap<Identifier> ID_TO_INDEX = Util.make(new Object2IntOpenHashMap<>(), idToIndex -> {
+			idToIndex.put(BUY_ITEMS_POOL, 0);
+			idToIndex.put(SELL_SPECIAL_ITEMS_POOL, 1);
+			idToIndex.put(SELL_COMMON_ITEMS_POOL, 2);
+		});
+
+		private static final Map<Identifier, TradeOffers.Factory[]> DELAYED_MODIFICATIONS = new HashMap<>();
+
+		/**
+		 * Make the trade list modifiable.
+		 */
+		static void initWanderingTraderTrades() {
+			if (!(TradeOffers.field_45129 instanceof ArrayList)) {
+				TradeOffers.field_45129 = new ArrayList<>(TradeOffers.field_45129);
+			}
+		}
+
+		@Override
+		public TradeOfferHelper.WanderingTraderOffersBuilder pool(Identifier id, int count, TradeOffers.Factory... factories) {
+			if (factories.length == 0) throw new IllegalArgumentException("cannot add empty pool");
+			if (count <= 0) throw new IllegalArgumentException("count must be positive");
+
+			Objects.requireNonNull(id, "id cannot be null");
+
+			if (ID_TO_INDEX.containsKey(id)) throw new IllegalArgumentException("pool id %s is already registered".formatted(id));
+
+			Pair<TradeOffers.Factory[], Integer> pool = Pair.of(factories, count);
+			initWanderingTraderTrades();
+			ID_TO_INDEX.put(id, TradeOffers.field_45129.size());
+			TradeOffers.field_45129.add(pool);
+			TradeOffers.Factory[] delayedModifications = DELAYED_MODIFICATIONS.remove(id);
+
+			if (delayedModifications != null) addOffersToPool(id, delayedModifications);
+
+			return this;
+		}
+
+		@Override
+		public TradeOfferHelper.WanderingTraderOffersBuilder addOffersToPool(Identifier pool, TradeOffers.Factory... factories) {
+			if (!ID_TO_INDEX.containsKey(pool)) {
+				DELAYED_MODIFICATIONS.compute(pool, (id, current) -> {
+					if (current == null) return factories;
+
+					return ArrayUtils.addAll(current, factories);
+				});
+				return this;
+			}
+
+			int poolIndex = ID_TO_INDEX.getInt(pool);
+			initWanderingTraderTrades();
+			Pair<TradeOffers.Factory[], Integer> poolPair = TradeOffers.field_45129.get(poolIndex);
+			TradeOffers.Factory[] modified = ArrayUtils.addAll(poolPair.getLeft(), factories);
+			TradeOffers.field_45129.set(poolIndex, Pair.of(modified, poolPair.getRight()));
+			return this;
+		}
 	}
 }
