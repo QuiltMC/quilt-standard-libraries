76368b9203a0fa6cecadcc1058f965eefb51146b
Eli Orona <eliorona@live.com>
2024-02-23 17:12:54 -0800

    Bump Version

--- build-logic/src/main/java/qsl/internal/Versions.java
+++ build-logic/src/main/java/qsl/internal/Versions.java
@@ -23,7 +23,7 @@ public final class Versions {
 	/**
 	 * The QSL version
 	 */
-	public static final String QSL_VERSION = "7.0.0-alpha.10";
+	public static final String QSL_VERSION = "7.0.0-alpha.11";
 
 	/**
 	 * The target Minecraft version.
