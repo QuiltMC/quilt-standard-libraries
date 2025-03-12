b2080d36c34ba037c36a2554f2b53af41287288d
alikindsys <alice@blocovermelho.org>
2025-03-12 16:51:14 -0300

    Use `WrapOperation` instead of `ModifyVariable`

--- library/item/item_content_registry/src/main/java/org/quiltmc/qsl/item/content/registry/mixin/FuelTimesMixin.java
+++ library/item/item_content_registry/src/main/java/org/quiltmc/qsl/item/content/registry/mixin/FuelTimesMixin.java
@@ -1,25 +1,28 @@
 package org.quiltmc.qsl.item.content.registry.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import it.unimi.dsi.fastutil.objects.Object2IntSortedMap;
-import it.unimi.dsi.fastutil.objects.Object2IntSortedMaps;
 import net.minecraft.block.entity.FuelTimes;
 import net.minecraft.item.Item;
-import org.objectweb.asm.Opcodes;
 import org.quiltmc.qsl.item.content.registry.impl.ItemContentRegistriesInitializer;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(targets = {"net.minecraft.block.entity.FuelTimes"})
+@Debug(export = true)
 public abstract class FuelTimesMixin {
-	@ModifyVariable(method = "<init>", at = @At(value = "FIELD", target = "net/minecraft/block/entity/FuelTimes.fuelTimes : Lit/unimi/dsi/fastutil/objects/Object2IntSortedMap;", opcode = Opcodes.PUTFIELD), argsOnly = true)
-	private Object2IntSortedMap<Item> returnCachedMap(Object2IntSortedMap<Item> old) {
+	@WrapOperation(method = "<init>", at = @At(value = "FIELD", target = "net/minecraft/block/entity/FuelTimes.fuelTimes : Lit/unimi/dsi/fastutil/objects/Object2IntSortedMap;"))
+	private void returnCachedMap(FuelTimes instance, Object2IntSortedMap<Item> value, Operation<Void> original) {
 		if(!ItemContentRegistriesInitializer.FUEL_MAP.isEmpty()) {
-			Object2IntSortedMap<Item> conversion = Object2IntSortedMaps.emptyMap();
-			conversion.putAll(ItemContentRegistriesInitializer.FUEL_MAP);
-			return conversion;
+		 	// Is this actually right? I guess it was done this way in order for it to not have duplicate entries
+			// But wouldn't the map sort it out for us?
+			// I'm keeping this here to make it functionally equivalent with the old mixin.
+			value.clear();
+			value.putAll(ItemContentRegistriesInitializer.FUEL_MAP);
 		}
-		return old;
+
+		original.call(instance, value);
 	}
 }
