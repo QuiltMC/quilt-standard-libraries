973c6359bb612231144999a5df62f9c4c4fe7f38
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-05-19 15:34:34 -0700

    update RegistryLibEventsTest, RegistryLibMonitorRegistrationTest, and RegistryLibMonitorTest

--- library/core/registry/src/testmod/java/org/quiltmc/qsl/registry/test/RegistryLibMonitorTest.java
+++ library/core/registry/src/testmod/java/org/quiltmc/qsl/registry/test/RegistryLibMonitorTest.java
@@ -28,6 +28,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.MapColor;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 import org.quiltmc.loader.api.ModContainer;
@@ -37,12 +39,18 @@ import org.quiltmc.qsl.registry.api.event.RegistryMonitor;
 public class RegistryLibMonitorTest implements ModInitializer {
 	private static final Logger LOGGER = LoggerFactory.getLogger("Quilt Registry Lib Monitor Test");
 
-	private static final Identifier TEST_BLOCK_A_ID = Identifier.of("quilt_registry_test_monitors", "test_block_a");
-	private static final Identifier TEST_BLOCK_B_ID = Identifier.of("quilt_registry_test_monitors", "test_block_b");
+	private static final RegistryKey<Block> TEST_BLOCK_A_KEY = RegistryKey.of(
+		RegistryKeys.BLOCK,
+		Identifier.of("quilt_registry_test_monitors", "test_block_a")
+	);
+	private static final RegistryKey<Block> TEST_BLOCK_B_KEY = RegistryKey.of(
+		RegistryKeys.BLOCK,
+		Identifier.of("quilt_registry_test_monitors", "test_block_b")
+	);
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		Block blockA = register(TEST_BLOCK_A_ID, new Block(AbstractBlock.Settings.copy(Blocks.STONE).mapColor(MapColor.BLACK)));
+		Block blockA = register(TEST_BLOCK_A_KEY, new Block(AbstractBlock.Settings.copy(Blocks.STONE).mapColor(MapColor.BLACK).key(TEST_BLOCK_A_KEY)));
 
 		var monitor = RegistryMonitor.create(Registries.BLOCK)
 				.filter(context -> context.id().getNamespace().equals("quilt_registry_test_monitors"));
@@ -61,7 +69,7 @@ public class RegistryLibMonitorTest implements ModInitializer {
 			upcomingSet.add(context.value());
 		});
 
-		Block blockB = register(TEST_BLOCK_B_ID, new Block(AbstractBlock.Settings.copy(Blocks.STONE).mapColor(MapColor.BLACK)));
+		Block blockB = register(TEST_BLOCK_B_KEY, new Block(AbstractBlock.Settings.copy(Blocks.STONE).mapColor(MapColor.BLACK).key(TEST_BLOCK_B_KEY)));
 
 		if (!allSet.contains(blockA) || !allSet.contains(blockB)) {
 			throw new AssertionError("Entries " + allSet + " found by RegistryMonitor via forAll were not as expected");
