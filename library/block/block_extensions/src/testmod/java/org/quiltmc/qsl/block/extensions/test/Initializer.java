f98e83e15c98b929eaa334b41b33056f175af667
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-06-17 17:09:19 -0700

    fix block_extensions testmod

--- library/block/block_extensions/src/testmod/java/org/quiltmc/qsl/block/extensions/test/Initializer.java
+++ library/block/block_extensions/src/testmod/java/org/quiltmc/qsl/block/extensions/test/Initializer.java
@@ -16,6 +16,7 @@
 
 package org.quiltmc.qsl.block.extensions.test;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.GlassBlock;
@@ -23,23 +24,50 @@ import net.minecraft.block.VineBlock;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 
-public final class Initializer implements ModInitializer {
-	public static final Block BLOCK = Registry.register(Registries.BLOCK,
-			Identifier.of("quilt_block_extensions_testmod", "test_block"),
-			new GlassBlock(QuiltBlockSettings.copyOf(Blocks.GLASS)
-					.luminance(15)
-					.pistonBehavior(PistonBehavior.PUSH_ONLY)));
+import java.util.function.Function;
 
-	public static final Block BLOCK2 = Registry.register(Registries.BLOCK,
-			Identifier.of("quilt_block_extensions_testmod", "test_block2"),
-			new VineBlock(QuiltBlockSettings.copyOf(Blocks.VINE).ticksRandomly(false)));
+public final class Initializer implements ModInitializer {
+	public static final String NAMESPACE = "quilt_block_extensions_testmod";
+
+	public static final Block BLOCK = registerBlock(
+		"test_block",
+		QuiltBlockSettings.copyOf(Blocks.GLASS),
+		settings -> new GlassBlock(settings
+			.luminance(15)
+			.pistonBehavior(PistonBehavior.PUSH_ONLY)
+		)
+	);
+
+	public static final Block BLOCK2 = registerBlock(
+		"test_block2",
+		QuiltBlockSettings.copyOf(Blocks.VINE),
+		settings -> new VineBlock(settings.ticksRandomly(false))
+	);
+
+	public static Identifier createId(String path) {
+		return Identifier.of(NAMESPACE, path);
+	}
+
+	public static RegistryKey<Block> createBlockKey(String path) {
+		return RegistryKey.of(RegistryKeys.BLOCK, createId(path));
+	}
+
+	private static <B extends Block, S extends AbstractBlock.Settings> B registerBlock(
+		String path, S baseSettings, Function<S, B> factory
+	) {
+		final RegistryKey<Block> key = createBlockKey(path);
+		baseSettings.key(key);
+		return Registry.register(Registries.BLOCK, key, factory.apply(baseSettings));
+	}
 
 	@Override
-	public void onInitialize(ModContainer mod) {}
+	public void onInitialize(ModContainer mod) { }
 }
